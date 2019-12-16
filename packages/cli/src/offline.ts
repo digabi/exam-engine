8e1bae010964b9803db8721e88de77ca2e3d3828
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2019-12-16 20:58:12 +0200

    Make offline output directory argument optional

--- packages/cli/src/offline.ts
+++ packages/cli/src/offline.ts
@@ -2,13 +2,14 @@
 
 import { createOfflineExam } from '@digabi/exam-engine-rendering'
 import { promises as fs } from 'fs'
+import path from 'path'
 import yargs from 'yargs'
 import { resolveExam } from './utils'
 
 // tslint:disable-next-line: no-unused-expression
 yargs
   .command(
-    '$0 <examFilename> <outputDirectory>',
+    '$0 <examFilename> [outputDirectory]',
     'Generates a static offline version of the exam ',
     yargsc =>
       yargsc
@@ -17,11 +18,12 @@ yargs
           coerce: resolveExam
         })
         .positional('outputDirectory', { description: 'The output directory', type: 'string' })
-        .demandOption(['examFilename', 'outputDirectory']),
+        .demandOption(['examFilename']),
     async ({ examFilename, outputDirectory }) => {
+      const finalOutputDirectory = outputDirectory ?? path.dirname(examFilename)
       try {
         await fs.access(examFilename)
-        await createOfflineExam(examFilename, outputDirectory)
+        await createOfflineExam(examFilename, finalOutputDirectory)
       } catch (err) {
         // tslint:disable-next-line: no-console
         console.error(err)
