4e241ab7eb05b8f129b7137cbc771e88b84aa5f2
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2019-12-18 13:22:21 +0200

    Change Tela scripts to use `ee`.

--- debian-packaging/afterinstall.sh
+++ debian-packaging/afterinstall.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 function afterinstall() {
+    # Add ee to $PATH
+    local NODE_VERSION=$(< /home/digabi/exam-engine/.nvmrc)
+    cat > /home/digabi/bin/ee << EOF
+#!/bin/bash
+
+/home/digabi/digabi-top/nvm/versions/node/v$NODE_VERSION/bin/node /home/digabi/exam-engine/packages/cli/dist/index.js "$@"
+EOF
+    chmod +x /home/digabi/bin/ee
+
     # Backwards compatibility: add a symlink to the old schema directory
-    rm /home/digabi/exam-engine/schema
-    ln -s /home/digabi/exam-engine/packages/mastering/schema /home/digabi/exam-engine/schema
+    ln -sf /home/digabi/exam-engine/packages/mastering/schema /home/digabi/exam-engine/schema
 
     rm -rf /home/digabi/Työpöytä/mex-demo
     mkdir /home/digabi/Työpöytä/mex-demo
 
+    # Link sample exams to the desktop.
     exams=( MexDocumentation FF A_X SC N )
     for exam in "${exams[@]}"
     do
@@ -16,7 +25,6 @@ function afterinstall() {
         sed -i 's|xsi:schemaLocation="http://ylioppilastutkinto.fi/exam.xsd ../../packages/mastering/schema/exam.xsd"|xsi:schemaLocation="http://ylioppilastutkinto.fi/exam.xsd file:///home/digabi/exam-engine/schema/exam.xsd"|' /home/digabi/Työpöytä/mex-demo/$exam/koe.xml
         sed -i 's|xsi:schemaLocation="http://ylioppilastutkinto.fi/exam.xsd https://abitti.dev/schema/exam.xsd"|xsi:schemaLocation="http://ylioppilastutkinto.fi/exam.xsd file:///home/digabi/exam-engine/schema/exam.xsd"|' /home/digabi/Työpöytä/mex-demo/$exam/koe.xml
         cp -r /home/digabi/exam-engine/packages/exams/$exam/attachments /home/digabi/Työpöytä/mex-demo/$exam
-        ln -s /home/digabi/exam-engine/bin/start-mex /home/digabi/Työpöytä/mex-demo/$exam/start-mex
     done
 
     cp -f /home/digabi/exam-engine/desktop/*.desktop "$(xdg-user-dir DESKTOP)"
