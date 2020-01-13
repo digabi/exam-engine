2103268a4593902f6c1d480e7fbe8fc41e3c3e6b
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2020-01-13 14:26:04 +0200

    Include display number for improved error messages when saving

--- packages/core/src/components/types.ts
+++ packages/core/src/components/types.ts
@@ -6,6 +6,7 @@ export type QuestionId = number
 interface AnswerCommon {
   questionId: QuestionId
   value: string
+  displayNumber?: string
 }
 export interface TextAnswer extends AnswerCommon {
   type: 'text'
