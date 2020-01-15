9506bf64f6b455beeafae13d710fc0fbcb5f97ef
Erik Lindroos <erik.lindroos@ylioppilastutkinto.fi>
2020-01-15 11:19:26 +0200

    Create I.ts
    Created translation file for Inari Sami.

---
+++ packages/core/src/i18n/I.ts
@@ -0,0 +1,17 @@
+import { ExamTranslations } from './fi-FI'
+
+export const I: ExamTranslations = {
+  'exam-total': 'Iskos ohtsis',
+  material: 'Amnâstuvah',
+  'external-material-title': 'Amnâstuvah',
+  part: 'Uási {{displayNumber}}:',
+  points: '{{count}} č.',
+  references: {
+    source: 'Käldee:',
+    date: 'Almostittum:',
+    'reference-date': 'Čujottum:',
+    translator: 'Jurgâlus:',
+    'modified-by': 'Heiviittâs:'
+  },
+  'toc-heading': 'Siskáldâs'
+}
