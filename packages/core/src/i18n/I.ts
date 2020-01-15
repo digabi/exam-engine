cd735bdc10447bb4f7429a7786a31e90a647cc23
Erik Lindroos <erik.lindroos@ylioppilastutkinto.fi>
2020-01-15 13:00:56 +0200

    Update I.ts
    Added mother tongue instructions.

--- packages/core/src/i18n/I.ts
+++ packages/core/src/i18n/I.ts
@@ -13,5 +13,18 @@ export const I: ExamTranslations = {
     translator: 'Jurgâlus:',
     'modified-by': 'Heiviittâs:'
   },
+  'answering-instructions': {
+    'toc-section_*_1_1': 'Fáádáh',
+    'section_*_1_1': 'Valjii oovtâ fáádá.'
+  },
   'toc-heading': 'Siskáldâs'
+}
+import { ExamTranslations } from './fi-FI'
+
+export const A_E: ExamTranslations = {
+  'external-material-title': 'Aineistot',
+  'answering-instructions': {
+    'toc-section_*_1_1': 'Aiheet',
+    'section_*_1_1': 'Valitse yksi aihe.'
+  }
 }
