f875f48e43f55a4e4491361ee7bca2351bad5549
Erik Lindroos <erik.lindroos@ylioppilastutkinto.fi>
2020-01-15 13:27:39 +0200

    Update I.ts

--- packages/core/src/i18n/I.ts
+++ packages/core/src/i18n/I.ts
@@ -19,12 +19,3 @@ export const I: ExamTranslations = {
   },
   'toc-heading': 'Siskáldâs'
 }
-import { ExamTranslations } from './fi-FI'
-
-export const A_E: ExamTranslations = {
-  'external-material-title': 'Aineistot',
-  'answering-instructions': {
-    'toc-section_*_1_1': 'Aiheet',
-    'section_*_1_1': 'Valitse yksi aihe.'
-  }
-}
