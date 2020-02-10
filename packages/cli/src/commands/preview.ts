999e78903c37baec07d64d829434f64cae7b3a2b
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-02-10 13:45:44 +0200

    Remove unnecessary lint exclusion

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -1,7 +1,6 @@
 import { previewExam } from '@digabi/exam-engine-rendering'
 import { Ora } from 'ora'
 
-// tslint:disable-next-line: no-unused-expression
 export default async function({ exam, spinner }: { exam: string; spinner: Ora }) {
   spinner.start(`Previewing ${exam}...`)
   const [url] = await previewExam(exam, { openFirefox: true })
