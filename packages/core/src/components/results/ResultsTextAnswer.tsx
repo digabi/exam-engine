2a5b11ca9caabe6dc808f94b2d331f9b53ca5a70
Ville Touronen <ville.touronen@reaktor.com>
2020-03-05 13:11:34 +0200

    Refactor single line answer numbering logic

--- packages/core/src/components/results/ResultsTextAnswer.tsx
+++ packages/core/src/components/results/ResultsTextAnswer.tsx
@@ -4,22 +4,22 @@ import { Translation } from 'react-i18next'
 import { getNumericAttribute } from '../../dom-utils'
 import { shortDisplayNumber } from '../../shortDisplayNumber'
 import AnswerToolbar from '../AnswerToolbar'
+import { QuestionContext } from '../QuestionContext'
 import { ExamComponentProps, TextAnswer } from '../types'
 import { findScore, ResultsContext } from './ResultsContext'
 import ResultsExamQuestionScore from './ResultsExamQuestionScore'
 
 function ResultsTextAnswer({ element, className }: ExamComponentProps) {
-  const { answersByQuestionId } = useContext(ResultsContext)
+  const { answerCount } = useContext(QuestionContext)
+  const { answersByQuestionId, scores } = useContext(ResultsContext)
   const questionId = getNumericAttribute(element, 'question-id')!
   const maxScore = getNumericAttribute(element, 'max-score')!
   const answer = answersByQuestionId[questionId] as TextAnswer | undefined
   const value = answer && answer.value
-  const { scores } = useContext(ResultsContext)
   const displayNumber = shortDisplayNumber(element.getAttribute('display-number')!)
   const score = findScore(scores, questionId)
   const comment = score?.comment
   const type = (element.getAttribute('type') || 'single-line') as 'rich-text' | 'multi-line' | 'single-line'
-  const hasSiblings = element!.parentElement!.getElementsByTagName('e:text-answer').length > 1
 
   switch (type) {
     case 'rich-text':
@@ -63,14 +63,14 @@ function ResultsTextAnswer({ element, className }: ExamComponentProps) {
     default:
       return (
         <>
-          {hasSiblings && <sup>{displayNumber}</sup>}
+          {answerCount > 1 && <sup>{displayNumber}</sup>}
           <span className={classNames('text-answer text-answer--single-line', className)}>{value}</span>
           {score && (
             <ResultsExamQuestionScore
               className="e-float-right"
               score={score.scoreValue}
               maxScore={maxScore}
-              displayNumber={hasSiblings ? displayNumber : undefined}
+              displayNumber={answerCount > 1 ? displayNumber : undefined}
             />
           )}
         </>
