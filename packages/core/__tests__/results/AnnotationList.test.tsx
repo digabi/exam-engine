a3459109a8cbc64c0aa093c4da1cc2dac8049936
Ville Touronen <ville.touronen@reaktor.com>
2020-03-26 15:17:46 +0200

    Fix calling AnnotationList without censor field in score object

---
+++ packages/core/__tests__/results/AnnotationList.test.tsx
@@ -0,0 +1,140 @@
+import * as i18next from 'i18next'
+import _ from 'lodash'
+import React from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { create } from 'react-test-renderer'
+import { Score } from '../../src'
+import { QuestionContext } from '../../src/components/QuestionContext'
+import AnnotationList from '../../src/components/results/AnnotationList'
+import { ResultsContext } from '../../src/components/results/ResultsContext'
+import { initI18n } from '../../src/i18n'
+
+function mkTextAnswer(questionId: number, displayNumber: string, maxScore: number): Element {
+  const textAnswer = document.createElement('text-answer')
+  textAnswer.setAttribute('type', 'rich-text')
+  textAnswer.setAttribute('max-score', String(maxScore))
+  textAnswer.setAttribute('question-number', displayNumber)
+  textAnswer.setAttribute('question-id', String(questionId))
+
+  return textAnswer
+}
+
+const defaultScores: Score = {
+  questionId: 1,
+  answerId: 1,
+  pregrading: {
+    score: 1,
+    annotations: [
+      {
+        startIndex: 0,
+        length: 1,
+        message: 'Test pregrading annotation'
+      }
+    ]
+  },
+  censoring: {
+    scores: [
+      { score: 4, shortCode: 'SE3' },
+      { score: 3, shortCode: 'SE2' },
+      { score: 2, shortCode: 'SE1' }
+    ],
+    annotations: [
+      {
+        startIndex: 0,
+        length: 1,
+        message: 'Test censoring annotation'
+      }
+    ]
+  },
+  inspection: { score: 5, shortCodes: ['IN1', 'IN2'] }
+}
+
+const defaultAnswer = mkTextAnswer(1, '5.1', 12)
+
+describe('<AnnotationList />', () => {
+  let i18n: i18next.i18n
+
+  describe('fi-FI', () => {
+    beforeAll(() => {
+      i18n = initI18n('fi-FI', null, null)
+    })
+
+    it('renders empty without score', () => {
+      const resultsProps = {
+        scores: []
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+
+    it('renders empty without annotations', () => {
+      const resultsProps = {
+        scores: [_.pick(defaultScores, 'answerId', 'questionId')]
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+
+    it('renders with only pregrading annotations', () => {
+      const resultsProps = {
+        scores: [_.pick(defaultScores, 'pregrading', 'answerId', 'questionId')]
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+
+    it('renders with only censoring annotations', () => {
+      const resultsProps = {
+        scores: [_.pick(defaultScores, 'censoring', 'answerId', 'questionId')]
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+
+    it('renders with pregrading and censoring annotations', () => {
+      const resultsProps = {
+        scores: [defaultScores]
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+  })
+
+  describe('sv-FI', () => {
+    beforeAll(() => {
+      i18n = initI18n('sv-FI', null, null)
+    })
+
+    it('renders with pregrading and censoring annotations', () => {
+      const resultsProps = {
+        scores: [defaultScores]
+      }
+      expect(renderWithContext(resultsProps, [defaultAnswer]).toJSON()).toMatchSnapshot()
+    })
+  })
+
+  const questionContextProps: QuestionContext = {
+    answers: [],
+    displayNumber: '1',
+    hasExternalMaterial: false,
+    maxAnswers: 2,
+    maxScore: 2,
+    level: 2,
+    childQuestions: []
+  }
+
+  const resultsContextDefaults: ResultsContext = {
+    answersByQuestionId: {},
+    gradingStructure: { questions: [] },
+    scores: [],
+    gradingText: '',
+    totalScore: 60
+  }
+
+  function renderWithContext(results: Partial<ResultsContext>, answers: Element[]) {
+    return create(
+      <QuestionContext.Provider value={{ ...questionContextProps, ...{ answers } }}>
+        <ResultsContext.Provider value={{ ...resultsContextDefaults, ...results }}>
+          <I18nextProvider i18n={i18n}>
+            <AnnotationList />
+          </I18nextProvider>
+        </ResultsContext.Provider>
+      </QuestionContext.Provider>
+    )
+  }
+})
--- packages/core/src/components/results/AnnotationList.tsx
+++ packages/core/src/components/results/AnnotationList.tsx
@@ -8,14 +8,56 @@ import { QuestionContext } from '../QuestionContext'
 import { Annotation, Score } from '../types'
 import { findScore, ResultsContext } from './ResultsContext'
 
-function ResultsAnnotationList() {
+interface AnnotationItem {
+  numbering: string
+  message: string
+}
+
+interface AnnotationListProps {
+  pregradingAnnotations: AnnotationItem[]
+  censoringAnnotations: AnnotationItem[]
+}
+
+export const ResultsAnnotationListComponent = ({
+  pregradingAnnotations,
+  censoringAnnotations
+}: AnnotationListProps) => {
   const { t } = useTranslation()
+
+  return pregradingAnnotations.length || censoringAnnotations.length ? (
+    <div className="e-annotation-list e-columns e-mrg-t-2">
+      <div className="e-column e-column--6">
+        <h5>{t('grading.pregrading-annotations')}</h5>
+        <ol className="e-list-data e-pad-l-0 e-font-size-s">
+          {pregradingAnnotations.map(({ numbering, message }) => (
+            <li data-list-number={numbering} key={numbering}>
+              {message}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div className="e-column e-column--6">
+        <h5>{t('grading.censor-annotations')}</h5>
+        <ol className="e-list-data e-pad-l-0 e-font-size-s">
+          {censoringAnnotations.map(({ numbering, message }) => (
+            <li data-list-number={numbering} key={numbering}>
+              {message}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  ) : null
+}
+
+function ResultsAnnotationList() {
   const { answers } = useContext(QuestionContext)
   const { scores } = useContext(ResultsContext)
 
   const answersAndScores = mapMaybe(answers, answer => {
     const questionId = getNumericAttribute(answer, 'question-id')
     const score = findScore(scores, questionId!)
+
     return score?.pregrading?.annotations?.length || score?.censoring?.annotations?.length
       ? ([answer, score] as const)
       : undefined
@@ -29,34 +71,22 @@ function ResultsAnnotationList() {
     annotationsFrom: 'pregrading' | 'censoring',
     listNumberOffset: number = 0
   ) =>
-    _.flatMap(answerElementAndScores, ([answer, score]) =>
-      score!
-        [annotationsFrom]!.annotations!.filter(a => a.message.length)
-        .map((annotation: Annotation, i: number) => {
-          const key = getPrefix(answer) + String(listNumberOffset + i + 1) + ')'
-          return (
-            <li data-list-number={key} key={key}>
-              {annotation.message}
-            </li>
-          )
-        })
+    _.flatMap(
+      answerElementAndScores,
+      ([answer, score]) =>
+        score![annotationsFrom]?.annotations
+          ?.filter(a => a.message.length)
+          .map((annotation: Annotation, i: number) => {
+            const numbering = getPrefix(answer) + String(listNumberOffset + i + 1) + ')'
+            const message = annotation.message
+            return { numbering, message }
+          }) ?? []
     )
 
   const pregradingAnnotations = getListOfAnnotations(answersAndScores, 'pregrading')
   const censoringAnnotations = getListOfAnnotations(answersAndScores, 'censoring', pregradingAnnotations.length)
 
-  return pregradingAnnotations.length || censoringAnnotations.length ? (
-    <div className="e-annotation-list e-columns e-mrg-t-2">
-      <div className="e-column e-column--6">
-        <h5>{t('grading.pregrading-annotations')}</h5>
-        <ol className="e-list-data e-pad-l-0 e-font-size-s">{pregradingAnnotations}</ol>
-      </div>
-      <div className="e-column e-column--6">
-        <h5>{t('grading.censor-annotations')}</h5>
-        <ol className="e-list-data e-pad-l-0 e-font-size-s">{censoringAnnotations}</ol>
-      </div>
-    </div>
-  ) : null
+  return <ResultsAnnotationListComponent {...{ pregradingAnnotations, censoringAnnotations }} />
 }
 
 export default React.memo(ResultsAnnotationList)
