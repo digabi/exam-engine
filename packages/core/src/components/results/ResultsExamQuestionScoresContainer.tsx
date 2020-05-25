b9795d287f381125955453242823fb1f3d1ef7da
Ville Touronen <ville.touronen@reaktor.com>
2020-05-25 16:25:56 +0300

    Limit the score component fix to just multi-line and rich-text answers

--- packages/core/src/components/results/ResultsExamQuestionScoresContainer.tsx
+++ packages/core/src/components/results/ResultsExamQuestionScoresContainer.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import classnames from 'classnames'
 
 function ResultsExamQuestionScoresContainer({
   answers,
   displayNumber,
   children,
+  className,
 }: {
   answers: Element[]
   displayNumber?: string
   children: React.ReactNode
+  className?: string
 }) {
   return (
-    <div className="e-result-scorecount e-float-right">
+    <div className={classnames('e-result-scorecount', 'e-float-right', className)}>
       <div className="e-result-scorecount-border-wrap">
         {answers.length > 1 && displayNumber && (
           <sup className="e-result-scorecount-sup e-mrg-r-1">{displayNumber}</sup>
