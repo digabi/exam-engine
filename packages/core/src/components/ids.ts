c69cae3f4c064d003cf946d30cd45ed0a792f9cf
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-07-21 16:12:34 +0300

    Remove space from attachment numbers
    For example, "1. A" -> "1.A"

--- packages/core/src/components/ids.ts
+++ packages/core/src/components/ids.ts
@@ -2,12 +2,10 @@ export const examTitleId = 'title'
 
 export const referencesTitleId = 'references-title'
 
-export const sectionTitleId = (displayNumber: string): string => `section-title-${trimWhitespace(displayNumber)}`
+export const sectionTitleId = (displayNumber: string): string => `section-title-${displayNumber}`
 
-export const questionTitleId = (displayNumber: string): string => `question-title-${trimWhitespace(displayNumber)}`
+export const questionTitleId = (displayNumber: string): string => `question-title-${displayNumber}`
 
-export const attachmentTitleId = (displayNumber: string): string => `attachment-title-${trimWhitespace(displayNumber)}`
+export const attachmentTitleId = (displayNumber: string): string => `attachment-title-${displayNumber}`
 
 export const tocTitleId = 'toc-title'
-
-const trimWhitespace = (str: string) => str.replace(/\s/g, '')
