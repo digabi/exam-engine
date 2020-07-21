8bb29487c9114fc217b2802e022a9dbced4eca87
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-07-21 15:22:43 +0300

    Improve accessibility of attachments

--- packages/core/src/components/AttachmentsAttachment.tsx
+++ packages/core/src/components/AttachmentsAttachment.tsx
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react'
 import { AttachmentContext, withAttachmentContext } from './AttachmentContext'
 import { ExamComponentProps } from '../createRenderChildNodes'
+import { attachmentTitleId } from './ids'
 
 function AttachmentsAttachment({ element, renderChildNodes }: ExamComponentProps) {
   const { displayNumber, isExternal } = useContext(AttachmentContext)
 
   return isExternal ? (
-    <figure id={displayNumber!} className="attachments-attachment e-mrg-0 e-mrg-t-8">
+    <section
+      id={displayNumber!}
+      className="attachments-attachment e-mrg-0 e-mrg-t-8"
+      aria-labelledby={attachmentTitleId(displayNumber!)}
+    >
       {renderChildNodes(element)}
-    </figure>
+    </section>
   ) : null
 }
 
--- packages/core/src/components/ids.ts
+++ packages/core/src/components/ids.ts
@@ -2,8 +2,12 @@ export const examTitleId = 'title'
 
 export const referencesTitleId = 'references-title'
 
-export const sectionTitleId = (displayNumber: string): string => `${displayNumber}-title`
+export const sectionTitleId = (displayNumber: string): string => `section-title-${trimWhitespace(displayNumber)}`
 
-export const questionTitleId = (displayNumber: string): string => `${displayNumber}-title`
+export const questionTitleId = (displayNumber: string): string => `question-title-${trimWhitespace(displayNumber)}`
+
+export const attachmentTitleId = (displayNumber: string): string => `attachment-title-${trimWhitespace(displayNumber)}`
 
 export const tocTitleId = 'toc-title'
+
+const trimWhitespace = (str: string) => str.replace(/\s/g, '')
