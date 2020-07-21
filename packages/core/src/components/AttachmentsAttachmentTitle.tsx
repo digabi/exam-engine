49945be08d1bb620373cc6d435cf4bb9d08112ac
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-07-21 15:22:43 +0300

    Improve accessibility of attachments

--- packages/core/src/components/AttachmentsAttachmentTitle.tsx
+++ packages/core/src/components/AttachmentsAttachmentTitle.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react'
 import { NBSP } from '../dom-utils'
 import { AttachmentContext } from './AttachmentContext'
 import { ExamComponentProps } from '../createRenderChildNodes'
+import { attachmentTitleId } from './ids'
 
 function AttachmentsAttachmentTitle({ element, renderChildNodes }: ExamComponentProps) {
   const { displayNumber } = useContext(AttachmentContext)
 
   return (
-    <h3>
+    <h3 id={attachmentTitleId(displayNumber!)}>
       {displayNumber}
       {NBSP}
       {renderChildNodes(element)}
