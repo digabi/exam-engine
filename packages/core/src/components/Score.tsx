7fae011750329cecb2c9629dbe63353f4c341881
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-07-22 14:28:36 +0300

    Improve accessibility of dropdown answers
    - Replace the vanilla <Downshift /> with useSelect, which is a better
      fit for building <select>-like components.
    - Make the toggle button a real <button>, so opening it with enter and
      spacebar works as expected.

--- packages/core/src/components/Score.tsx
+++ packages/core/src/components/Score.tsx
@@ -2,10 +2,13 @@ import classNames from 'classnames'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-export const Score: React.FunctionComponent<{ size?: 'inline' | 'small' | 'large'; score: number }> = ({
-  size = 'small',
-  score,
-}) => {
+export interface ScoreProps {
+  id?: string
+  size?: 'inline' | 'small' | 'large'
+  score: number
+}
+
+export const Score: React.FunctionComponent<ScoreProps> = ({ id, size = 'small', score }) => {
   const { t } = useTranslation()
   const Tag = size === 'inline' ? 'sup' : 'span'
   return (
@@ -15,6 +18,7 @@ export const Score: React.FunctionComponent<{ size?: 'inline' | 'small' | 'large
         'e-score--large e-font-size-l e-semibold': size === 'large',
         'e-score--inline e-font-size-xs': size === 'inline',
       })}
+      id={id}
     >
       {t('points', { count: score })}
     </Tag>
--- packages/core/src/components/ids.ts
+++ packages/core/src/components/ids.ts
@@ -12,4 +12,6 @@ export const externalMaterialListTitleId = (id: string): string => `external-mat
 
 export const tocSectionTitleId = (id: string): string => `toc-section-title-${id}`
 
+export const dropdownAnswerScoreId = (id: string): string => `dropdown-answer-score-${id}`
+
 export const tocTitleId = 'toc-title'
