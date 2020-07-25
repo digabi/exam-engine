52d4637257bfe97241ae3e639e358a26280e105a
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-07-25 23:16:48 +0300

    Enable isolatedModules to increase Jest performance

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
-  reporters: ['default', ['jest-junit', { outputName: 'jest-report.xml' }]],
+  globals: {'ts-jest': {isolatedModules: true}},
+  reporters: ['default', ['jest-junit', {outputName: 'jest-report.xml'}]],
   snapshotSerializers: ['jest-snapshot-serializer-raw'],
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/test*.ts?(x)', '<rootDir>/__tests__/**/*.test.ts?(x)']
+  testMatch: [
+    '<rootDir>/__tests__/**/test*.ts?(x)',
+    '<rootDir>/__tests__/**/*.test.ts?(x)'
+  ],
 }
