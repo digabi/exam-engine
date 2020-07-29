18e19c95d5a9721e07a1389a26b4997f3780ef35
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-07-29 08:51:20 +0300

    New ts-jest improves the performance of isolatedModules: false
    Let's try it out on CI.

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
-  globals: { 'ts-jest': { isolatedModules: true } },
   reporters: ['default', ['jest-junit', { outputName: 'jest-report.xml' }]],
   snapshotSerializers: ['jest-snapshot-serializer-raw'],
   testEnvironment: 'node',
