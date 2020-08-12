58d214bc7eba197cc6ea97f0c2fe1d21d5bc60c3
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-08-12 13:27:26 +0300

    Improve the accessibility of dropdown-answers
    - Mark where the answer begins and ends
    - Hide the correct answer markers from screen readers for now. Not sure
      how to represent them yet.
    - As a bugfix, render HTML content of dropdown-answer-options properly.

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -15,3 +15,16 @@ export function mapMaybe<T, U>(array: T[], fn: (value: T, index: number) => U | 
 
   return result
 }
+
+export function intersperse<T>(separator: T, array: T[]): T[] {
+  const result = new Array<T>(array.length * 2 - 1)
+
+  for (let i = 0; i < array.length; i++) {
+    result[i * 2] = array[i]
+    if (i !== array.length - 1) {
+      result[i * 2 + 1] = separator
+    }
+  }
+
+  return result
+}
