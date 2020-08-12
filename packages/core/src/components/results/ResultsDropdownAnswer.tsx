d701e3e4b9affb842e2ba295d7330730db2ce4ce
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-08-12 13:27:26 +0300

    Improve the accessibility of dropdown-answers
    - Mark where the answer begins and ends
    - Hide the correct answer markers from screen readers for now. Not sure
      how to represent them yet.
    - As a bugfix, render HTML content of dropdown-answer-options properly.

--- packages/core/src/components/results/ResultsDropdownAnswer.tsx
+++ packages/core/src/components/results/ResultsDropdownAnswer.tsx
@@ -1,38 +1,39 @@
 import classNames from 'classnames'
-import * as _ from 'lodash-es'
-import React, { useContext } from 'react'
-import { findChildElement, getNumericAttribute, mapChildElements } from '../../dom-utils'
+import React, { ReactNode, useContext } from 'react'
+import { findChildElement, getNumericAttribute } from '../../dom-utils'
 import { shortDisplayNumber } from '../../shortDisplayNumber'
 import { QuestionContext } from '../QuestionContext'
 import { findMultiChoiceFromGradingStructure, ResultsContext } from './ResultsContext'
 import ResultsExamQuestionAutoScore from './ResultsExamQuestionAutoScore'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { ChoiceAnswer } from '../..'
+import { useTranslation } from 'react-i18next'
+import { ScreenReaderOnly } from '../ScreenReaderOnly'
+import { intersperse } from '../../utils'
+import * as _ from 'lodash-es'
 
-function ResultsDropdownAnswer({ element }: ExamComponentProps) {
+function ResultsDropdownAnswer({ element, renderChildNodes }: ExamComponentProps) {
+  const { t } = useTranslation()
   const { answersByQuestionId, gradingStructure } = useContext(ResultsContext)
   const { answers } = useContext(QuestionContext)
   const questionId = getNumericAttribute(element, 'question-id')!
   const answer = answersByQuestionId[questionId] as ChoiceAnswer | undefined
 
-  const currentlySelectedItem =
-    answer &&
-    answer.value &&
-    findChildElement(element, (childElement) => answer.value === childElement.getAttribute('option-id'))
+  const selectedOption = findChildElement(
+    element,
+    (childElement) => answer?.value === childElement.getAttribute('option-id')
+  )
 
   const choice = findMultiChoiceFromGradingStructure(gradingStructure, questionId)!
 
-  if (currentlySelectedItem) {
+  if (selectedOption) {
     const correctIds = choice.options.filter((o) => o.correct).map((o) => o.id)
 
-    const correctAnswers = _.compact(
-      mapChildElements(element, (childElement) => {
-        if (correctIds.includes(getNumericAttribute(childElement, 'option-id') as number)) {
-          return childElement.textContent
-        }
-      })
+    const correctOptions = Array.from(element.children).filter((childElement) =>
+      correctIds.includes(getNumericAttribute(childElement, 'option-id')!)
     )
-    const isAnswerCorrect = correctIds.includes(getNumericAttribute(currentlySelectedItem, 'option-id') as number)
+
+    const isAnswerCorrect = correctIds.includes(getNumericAttribute(selectedOption, 'option-id') as number)
     const displayNumber = shortDisplayNumber(element.getAttribute('display-number')!)
     const scoreValue = answer && choice.options.find((option) => option.id === Number(answer.value))!.score
 
@@ -48,9 +49,16 @@ function ResultsDropdownAnswer({ element }: ExamComponentProps) {
             'e-dropdown-answer__answered--wrong': !isAnswerCorrect,
           })}
         >
-          {currentlySelectedItem.textContent}
+          <ScreenReaderOnly>{t('screen-reader.answer-begin')}</ScreenReaderOnly>
+          {renderChildNodes(selectedOption)}
+          <ScreenReaderOnly>{t('screen-reader.answer-end')}</ScreenReaderOnly>
+          {isAnswerCorrect && <ScreenReaderOnly>{t('screen-reader.correct-answer')}</ScreenReaderOnly>}
         </span>
-        {!isAnswerCorrect && <span className="e-dropdown-answer__correct">{correctAnswers.join(',')}</span>}
+        {!isAnswerCorrect && (
+          <span className="e-dropdown-answer__correct" aria-hidden={true}>
+            {intersperse(', ', _.flatMap<Element, ReactNode>(correctOptions, _.unary(renderChildNodes)))}
+          </span>
+        )}
         {scoreValue != null && (
           <ResultsExamQuestionAutoScore score={scoreValue} maxScore={maxScore} displayNumber={displayNumber} />
         )}
