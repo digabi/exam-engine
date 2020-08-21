14455468b2efb667b7ac5b5db69852882dce0c3d
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2020-08-21 12:28:03 +0300

    Add unit tests for intersperse()

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,7 +1,7 @@
 /**
  * Maps an array, skipping the value if `fn` returns `undefined`.
  *
- * `mapMaybe(array, fn) ≡ array.map(fn).filter(v => v !== undefined)`
+ * @example mapMaybe(array, fn) ≡ array.map(fn).filter(v => v !== undefined)
  */
 export function mapMaybe<T, U>(array: T[], fn: (value: T, index: number) => U | undefined): U[] {
   const result: U[] = []
@@ -16,15 +16,21 @@ export function mapMaybe<T, U>(array: T[], fn: (value: T, index: number) => U | 
   return result
 }
 
+/**
+ * Insert a separator between each element of the array.
+ *
+ * @example intersperse(',', ['a', 'b', 'c']) ≡ ['a', ',', 'b', ',' 'c']
+ */
 export function intersperse<T>(separator: T, array: T[]): T[] {
+  if (array.length === 0) return []
   const result = new Array<T>(array.length * 2 - 1)
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     result[i * 2] = array[i]
-    if (i !== array.length - 1) {
-      result[i * 2 + 1] = separator
-    }
+    result[i * 2 + 1] = separator
   }
+
+  result[result.length - 1] = array[array.length - 1]
 
   return result
 }
