66cdc2efac51d6c77c06778eda4e0011474a3064
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-08-31 13:13:27 +0300

    Add optional argument to choose preview port

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -1,9 +1,17 @@
 import { previewExam } from '@digabi/exam-engine-rendering'
 import { Ora } from 'ora'
 
-export default async function preview({ exam, spinner }: { exam: string; spinner: Ora }): Promise<string> {
+export default async function preview({
+  exam,
+  port,
+  spinner,
+}: {
+  exam: string
+  port?: number
+  spinner: Ora
+}): Promise<string> {
   spinner.start(`Previewing ${exam}...`)
-  const ctx = await previewExam(exam, { openFirefox: true })
+  const ctx = await previewExam(exam, { openFirefox: true, port: port })
   spinner.info(`Server is running at ${ctx.url}`)
   return `Press Ctrl-C to stop`
 }
