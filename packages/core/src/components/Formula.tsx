c4bfba38e116457809c17f3b928ff24b5858c9a0
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-01-12 16:14:21 +0200

    Render display formula as a block element
    Seems like this was accidentally changed in 910744d2200.

--- packages/core/src/components/Formula.tsx
+++ packages/core/src/components/Formula.tsx
@@ -5,9 +5,15 @@ import { ExamComponentProps } from '../createRenderChildNodes'
 function Formula({ element, className }: ExamComponentProps) {
   const svg = element.getAttribute('svg')!
   const mml = element.getAttribute('mml')!
+  const isDisplayFormula = element.getAttribute('mode') === 'display'
 
   const html = svg + '<span class="e-screen-reader-only" role="presentation">' + mml + '</span>'
-  return <span className={classNames('e-formula', className)} dangerouslySetInnerHTML={{ __html: html }} />
+  return (
+    <span
+      className={classNames('e-formula', { 'e-block': isDisplayFormula }, className)}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
 }
 
 export default React.memo(Formula)
