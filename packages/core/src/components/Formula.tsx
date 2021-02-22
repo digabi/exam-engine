90d75ecdeed7c0e31a826abf8acee9267bd621df
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-02-22 13:56:12 +0200

    Add an optional assistive-title attribute to e:formula.
    This attribute can be used to customize the screen reader representation
    of a formula. By default, we hide the SVG image with aria-hidden and
    show the LaTeX source of the formula in a sibling screen reader only
    element.  With the assistive-title attribute, the screen reader
    representation can be customized with e.g. an ASCIIMath or Unicode
    version of the formula.

--- packages/core/src/components/Formula.tsx
+++ packages/core/src/components/Formula.tsx
@@ -1,16 +1,22 @@
 import classNames from 'classnames'
 import React from 'react'
 import { ExamComponentProps } from '../createRenderChildNodes'
+import { getAttribute } from '../dom-utils'
 
 function Formula({ element, className }: ExamComponentProps) {
-  const svg = element.getAttribute('svg')!
-  const isDisplayFormula = element.getAttribute('mode') === 'display'
+  const svg = getAttribute(element, 'svg')!
+  const assistiveTitle = getAttribute(element, 'assistive-title')
+  const isDisplayFormula = getAttribute(element, 'mode') === 'display'
 
   return (
-    <span
-      className={classNames('e-formula', { 'e-block': isDisplayFormula }, className)}
-      dangerouslySetInnerHTML={{ __html: svg }}
-    />
+    <>
+      <span
+        className={classNames('e-formula', { 'e-block': isDisplayFormula }, className)}
+        dangerouslySetInnerHTML={{ __html: svg }}
+        aria-hidden="true"
+      />
+      <span className="e-screen-reader-only">{assistiveTitle || element.textContent}</span>
+    </>
   )
 }
 
