cf2f8148cf501e28177ed30d20604cc46704f3ca
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-03-05 11:11:22 +0200

    Re-add jest-junit
    I forgot that the Jenkins tests use it, so we can't remove it after all.

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  reporters: ['default', ['jest-junit', { outputName: 'jest-report.xml' }]],
   snapshotSerializers: ['jest-snapshot-serializer-raw'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/__tests__/**/test*.ts?(x)', '<rootDir>/__tests__/**/*.test.ts?(x)'],
