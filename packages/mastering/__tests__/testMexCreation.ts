ac26df049a706bbd64ff5ff8c83301e344cbbd47
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2021-04-14 02:55:50 +0300

    testMexCreation.ts: verify a signature

--- packages/mastering/__tests__/testMexCreation.ts
+++ packages/mastering/__tests__/testMexCreation.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import yauzl from 'yauzl-promise'
 import { Readable, PassThrough } from 'stream'
 import { createMex, AttachmentFile, createMultiMex, ExamFile } from '../dist/createMex'
+import { verifyWithSHA256AndRSA } from '../src/crypto-utils'
 
 interface InterestingEntryMetadata {
   fileName: string
@@ -11,12 +12,14 @@ interface InterestingEntryMetadata {
 
 describe('Mex exam package creation', () => {
   let privateKey: string
+  let publicKey: string
   let nsaScripts: Readable
   let securityCodes: Readable
   let exams: ExamFile[]
 
   beforeAll(async () => {
     privateKey = await readResource('private-key.pem')
+    publicKey = await readResource('public-key.pem')
   })
 
   beforeEach(() => {
@@ -52,11 +55,12 @@ describe('Mex exam package creation', () => {
   async function expectZipEntriesAreCorrect(
     mexBuffers: Buffer[],
     entryMapper: (value: yauzl.Entry, index: number, array: yauzl.Entry[]) => InterestingEntryMetadata
-  ) {
+  ): Promise<yauzl.Entry[]> {
     const zip = await yauzl.fromBuffer(Buffer.concat(mexBuffers))
     const rawEntries = await zip.readEntries()
     const mappedEntries = rawEntries.map(entryMapper)
     expect(mappedEntries).toMatchSnapshot()
+    return rawEntries
   }
 
   describe('with createMex', () => {
@@ -74,14 +78,23 @@ describe('Mex exam package creation', () => {
         ktpUpdate,
         koeUpdate
       )
-      await expectZipEntriesAreCorrect(mexBuffers, (e) => ({
+      return await expectZipEntriesAreCorrect(mexBuffers, (e) => ({
         fileName: e.fileName,
         uncompressedSize: e.fileName === 'rendering.zip.bin' ? 0 : e.uncompressedSize, // ignore rendering.zip size
       }))
     }
 
-    it('creates a minimal mex', async () => {
-      await expectCorrectMexIsCreated()
+    it('creates a minimal mex with verifyable exam.xml signature', async () => {
+      const mexEntries = await expectCorrectMexIsCreated()
+      const encryptedExamXml = await toBuffer(
+        await mexEntries.find((e) => e.fileName === 'exam.xml.bin')!.openReadStream()
+      )
+      const encryptedExamXmlSignature = (
+        await toBuffer(await mexEntries.find((e) => e.fileName === 'exam.xml.bin.sig')!.openReadStream())
+      ).toString('utf8')
+
+      const signatureVerificationResult = verifyWithSHA256AndRSA(encryptedExamXml, publicKey, encryptedExamXmlSignature)
+      expect(signatureVerificationResult).toBeTruthy()
     })
 
     it('creates a mex with koe-update.zip', async () => {
@@ -123,4 +136,12 @@ describe('Mex exam package creation', () => {
 
 async function readResource(filename: string): Promise<string> {
   return fs.readFile(path.resolve(__dirname, 'resources', filename), 'utf-8')
+}
+
+function toBuffer(stream: Readable): Promise<Buffer> {
+  return new Promise((resolve) => {
+    const buffers: Buffer[] = []
+    stream.on('data', (d) => buffers.push(d))
+    stream.on('end', () => resolve(Buffer.concat(buffers)))
+  })
 }
--- packages/mastering/src/crypto-utils.ts
+++ packages/mastering/src/crypto-utils.ts
@@ -28,6 +28,12 @@ export function signWithSHA256AndRSA(input: NodeJS.ReadableStream, privateKeyPem
   return output
 }
 
+export function verifyWithSHA256AndRSA(signedDataBuffer: Buffer, publicKey: string, signature: string): boolean {
+  const verifier = crypto.createVerify(signAlgo)
+  verifier.update(signedDataBuffer)
+  return verifier.verify(publicKey, signature, 'base64')
+}
+
 export function deriveAES256KeyAndIv(password: string): KeyAndIv {
   const trimmedPassword = password.replace(/\s/g, '')
   const derivedData = crypto.pbkdf2Sync(trimmedPassword, trimmedPassword, 2000, 32 + 16, defaultDigest) // Use password as salt as well, 32 bytes for key, 16 for iv
