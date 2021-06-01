0643e2061a0db8b13d19ffd3629f370fbe07dc14
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-06-01 12:36:12 +0300

    Do not render grading instruction audio elements without transcriptions

--- packages/core/src/components/grading-instructions/Audio.tsx
+++ packages/core/src/components/grading-instructions/Audio.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
+import { findChildElement } from '../../dom-utils'
 
 export const Audio: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
-  return <details className="e-grading-instruction-audio e-pad-1 e-mrg-b-2">{renderChildNodes(element)}</details>
+  const hasTranscription = findChildElement(element, 'audio-transcription') != null
+
+  return hasTranscription ? (
+    <details className="e-grading-instruction-audio e-pad-1 e-mrg-b-2">{renderChildNodes(element)}</details>
+  ) : null
 }
 
 export default Audio
