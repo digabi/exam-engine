378060e7e2e137101d65d0cbd07ca32ac53feeb6
Eero Anttila <eea@iki.fi>
2021-06-01 10:17:32 +0300

    Show audio element only when transcription available

--- packages/core/src/components/grading-instructions/Audio.tsx
+++ packages/core/src/components/grading-instructions/Audio.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 
 export const Audio: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
-  return <details className="e-grading-instruction-audio e-pad-1 e-mrg-b-2">{renderChildNodes(element)}</details>
+  return Array.from(element.childNodes).some((x) => x.nodeName === 'e:audio-transcription') ? (
+    <details className="e-grading-instruction-audio e-pad-1 e-mrg-b-2">{renderChildNodes(element)}</details>
+  ) : (
+    <></>
+  )
 }
 
 export default Audio
