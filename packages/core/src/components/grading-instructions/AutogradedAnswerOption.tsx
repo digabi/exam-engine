112eb67716012ff9b0a1514f54f62f4358c2f485
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-06-02 12:00:09 +0300

    grading-instructions: show answer option points in plaintext

--- packages/core/src/components/grading-instructions/AutogradedAnswerOption.tsx
+++ packages/core/src/components/grading-instructions/AutogradedAnswerOption.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { getNumericAttribute } from '../../dom-utils'
-import { Score } from '../shared/Score'
+import { useExamTranslation } from '../../i18n'
 
 const AutogradedAnswerOption: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
   const score = getNumericAttribute(element, 'score')
   if (!score) return null
 
+  const { t } = useExamTranslation()
+
   return (
     <li>
-      {renderChildNodes(element)} <Score score={score} />
+      {renderChildNodes(element)} ({t('points', { count: score })})
     </li>
   )
 }
