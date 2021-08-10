29468feff8b5b12e902dd2063716c96045d39aa5
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-08-10 11:47:47 +0300

    Remove e-break-word class from exam grading instructions
    Not quite sure why I had added it there in the first place (likely a
    copy-paste brainfart), but it caused problems with table line breaks.

--- packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 
 const ExamGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
-  return <div className="e-exam-grading-instruction e-break-word notification e-pad-4">{renderChildNodes(element)}</div>
+  return <div className="e-exam-grading-instruction notification e-pad-4">{renderChildNodes(element)}</div>
 }
 
 export default ExamGradingInstruction
