459fb1e793a4c9f397dd487e1fc2bf8acb159e80
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2021-09-06 13:15:54 +0300

    Fix setting of document title in preview
    The useEffect hook is run before the result has been "committed" to the
    DOM, so it ends up being run too early when switching languages. By
    replacing it with useLayoutEffect, the issue is resolved.

--- packages/core/src/components/DocumentTitle.tsx
+++ packages/core/src/components/DocumentTitle.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 
 type DocumentTitleProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>
 
 /** A helper component that sets document.title based on its contents. */
 const DocumentTitle: React.FunctionComponent<DocumentTitleProps> = (props) => {
   const ref = useRef<HTMLHeadingElement>(null)
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (ref.current) {
       document.title = ref.current.textContent!
     }
