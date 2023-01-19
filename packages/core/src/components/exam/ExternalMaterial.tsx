1e02098e616db6bf7f79fd863e52f76bdc12749d
Sami Saves <sami.saves@reaktor.com>
2023-01-19 08:57:57 +0200

    Change external material title to h4 for accessibility

--- packages/core/src/components/exam/ExternalMaterial.tsx
+++ packages/core/src/components/exam/ExternalMaterial.tsx
@@ -49,9 +49,9 @@ function ExternalMaterial({ element, showTitle = true, forceRender = false }: Ex
   return (
     <div className="external-material-list e-mrg-b-4">
       {showTitle && (
-        <div className="e-font-size-l e-semibold" id={id}>
+        <h4 className="external-material-title" id={id}>
           {t('external-material-title')}
-        </div>
+        </h4>
       )}
       <ol className="e-list-data e-pad-l-0 e-mrg-0" aria-labelledby={id}>
         {renderChildNodes(element)}
