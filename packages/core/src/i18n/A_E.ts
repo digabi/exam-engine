c40c9f5102de2c17f6fce3d5441737c02f47ee3d
Sami Saves <sami.saves@reaktor.com>
2023-01-24 11:52:59 +0200

    Fix A_E exams having wrong instructions in TOC

--- packages/core/src/i18n/A_E.ts
+++ packages/core/src/i18n/A_E.ts
@@ -3,6 +3,7 @@ import { ExamTranslations } from './fi-FI'
 export const A_E: ExamTranslations = {
   'external-material-title': 'Aineistot',
   'answering-instructions': {
+    'toc-section_*_1_1': 'Valitse yksi aihe.',
     'section_*_1_1': 'Valitse yksi aihe.',
   },
 }
--- packages/core/src/i18n/O_E.ts
+++ packages/core/src/i18n/O_E.ts
@@ -2,6 +2,7 @@ import { ExamTranslations } from './fi-FI'
 
 export const O_E: ExamTranslations = {
   'answering-instructions': {
+    'toc-section_*_1_1': 'Välj ett ämne.',
     'section_*_1_1': 'Välj ett ämne.',
   },
 }
