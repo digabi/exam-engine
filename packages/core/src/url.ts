7867bb95e0dcbe944e4d07e3f30f23924dbaeee4
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2023-01-26 15:26:56 +0200

    Bump version

--- packages/core/src/url.ts
+++ packages/core/src/url.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash-es'
 type URLOptions = Partial<Omit<URL, 'origin' | 'searchParams' | 'toJSON'>>
 
 /**
- * A helper function for constructing relative URLs for links within the exam.
+ * A helper function for constructing relative URLs for links within the exam
  */
 export function url(path: string, options?: URLOptions): string {
   const u = new URL(path, 'http://example.com') // hostname doesn't matter but it is required for URL constructor
