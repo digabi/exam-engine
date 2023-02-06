1f3117fb31add9789a74dbae0069d64baa03d754
Eero Anttila <eea@iki.fi>
2023-02-06 17:02:14 +0200

    Show answers one at a time

--- packages/core/src/components/grading/Grading.tsx
+++ packages/core/src/components/grading/Grading.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { changeLanguage, initI18n } from '../../i18n'
 import { useCached } from '../../useCached'
@@ -16,23 +16,34 @@ const Results: React.FunctionComponent<CommonExamProps> = () => {
   const i18n = useCached(() => initI18n('FI-fi'))
   useEffect(changeLanguage(i18n, 'FI-fi'))
 
+  const answerIds = Object.keys(answersByQuestionId).map(Number)
+  const [answerId, setAnswerId] = useState<number>(answerIds[0])
+
+  const { questionId, type, value, displayNumber } = answersByQuestionId[answerId]
+  function selectQuestion(id: number) {
+    setAnswerId(id)
+  }
   return (
     <I18nextProvider i18n={i18n}>
       <main className="e-exam">
-        {Object.values(answersByQuestionId).map(({ questionId, type, value, displayNumber }) => (
-          <div key={questionId}>
-            <div>
-              {displayNumber} ({questionId})
-            </div>
-            {type === 'richText' ? (
-              <MultiLineAnswer {...{ key: questionId, type: 'rich-text', value }} />
-            ) : (
-              <SingleLineAnswer {...{ key: questionId, value, answers }}>
-                <div></div>
-              </SingleLineAnswer>
-            )}
-          </div>
-        ))}
+        <div>
+          {answerIds.map((id) => (
+            <button onClick={() => selectQuestion(id)} key={id}>
+              {id}
+            </button>
+          ))}
+        </div>
+
+        <div>
+          Tehtävä {displayNumber} ({questionId})
+        </div>
+        {type === 'richText' ? (
+          <MultiLineAnswer {...{ type: 'rich-text', value }} />
+        ) : (
+          <SingleLineAnswer {...{ value, answers }}>
+            <div></div>
+          </SingleLineAnswer>
+        )}
       </main>
     </I18nextProvider>
   )
