c48e861f744e7f6c83ee3d65a44dc25a587092cd
Eero Anttila <eea@iki.fi>
2023-02-22 15:28:46 +0200

    Try making image status updater error proof

--- packages/core/src/components/grading/largeImageDetector.ts
+++ packages/core/src/components/grading/largeImageDetector.ts
@@ -25,16 +25,16 @@ export function updateLargeImageWarnings(answer: Element) {
 }
 function updateImageStatus(img: HTMLImageElement) {
   remainingImages--
-  const wrapper = img.parentElement!
-  const nextSibling = wrapper.nextSibling
+  const wrapper = img.parentElement
+  const nextSibling = wrapper?.nextSibling
   const hasFullSizeLink = nextSibling instanceof HTMLElement && nextSibling.classList.contains('full-size-image')
   if (img.naturalWidth > img.width) {
     if (!hasFullSizeLink) {
-      wrapper.insertAdjacentHTML('afterend', `<div class="full-size-image"><a target="_blank" href="${img.src}"></a>`)
+      wrapper?.insertAdjacentHTML('afterend', `<div class="full-size-image"><a target="_blank" href="${img.src}"></a>`)
     }
   } else {
     if (hasFullSizeLink) {
-      nextSibling.remove()
+      nextSibling?.remove()
     }
   }
 }
