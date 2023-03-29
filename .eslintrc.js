ecb190e74995534b0b28b526c90ad142d8fc301e
Eero Anttila <eea@iki.fi>
2023-03-29 15:12:02 +0300

    Convert eslintrc from js to json

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: '@digabi/eslint-config',
-  parserOptions: {
-    project: ['./packages/*/tsconfig.json', './packages/*/__tests__/tsconfig.json'],
-  },
-  ignorePatterns: ['packages/*/dist', '**/node_modules', '**/main-bundle.js'],
-}
