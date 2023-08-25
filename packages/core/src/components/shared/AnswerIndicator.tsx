cdfd84a9561031bb2c8166854801afd0231d1baa
Eero Anttila <eea@iki.fi>
2023-08-25 13:16:14 +0300

    Calculate correctly screenshots and formulas

--- packages/core/src/components/shared/AnswerIndicator.tsx
+++ packages/core/src/components/shared/AnswerIndicator.tsx
@@ -14,16 +14,18 @@ type Props = {
 const AnswerIndicator = (props: Props) => {
   const { id, type, answer, error } = props
 
-  const answerIsFormula = answer?.value.includes('math.svg')
-  const answerIsImage = answer?.value.includes('data:image')
+  const value = answer?.value
   const answerIsLongText = answer?.type === 'richText'
+  const answerIsFormula = answerIsLongText && value?.includes('src="/math.svg?latex=')
+  const answerIsImage =
+    answerIsLongText && (value?.includes('<img src="/screenshot/') || value?.includes('<img src="data:image/png;'))
 
   return (
     <>
       <div
         key={id}
         className={classNames('answer-indicator', {
-          ok: answer?.value,
+          ok: value,
           error,
           big: type === 'rich-text'
         })}
@@ -31,7 +33,7 @@ const AnswerIndicator = (props: Props) => {
       >
         {answer?.type === 'richText' && (
           <>
-            {answerIsLongText && <span>{answer?.characterCount || ''}</span>}
+            {(answerIsLongText && answer?.characterCount && <span>{answer.characterCount}</span>) || ''}
             {answerIsFormula && <span className="formula">∑</span>}
             {answerIsImage && (
               <span className="img">
