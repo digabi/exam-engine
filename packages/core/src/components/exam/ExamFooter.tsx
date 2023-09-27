7333d1053d07318773fe3f5915cc5e9638c99553
Heikki Mantere <heikki.mantere@gmail.com>
2023-09-27 13:03:25 +0300

    Add finishExam to ExamFooter component

--- packages/core/src/components/exam/ExamFooter.tsx
+++ packages/core/src/components/exam/ExamFooter.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SectionElement from '../SectionElement'
 import { ExamComponentProps } from '../../createRenderChildNodes'
+import { ExamContext } from '../context/ExamContext'
+import { useExamTranslation } from '../../i18n'
 
 function ExamFooter({ element, renderChildNodes }: ExamComponentProps) {
-  return <SectionElement>{renderChildNodes(element)}</SectionElement>
+  const { examServerApi } = useContext(ExamContext)
+  const { finishExam } = examServerApi
+  const { t } = useExamTranslation()
+
+  return (
+    <SectionElement>
+      <div className="e-exam-footer">
+        {renderChildNodes(element)}
+        <button id="finishExam" onClick={() => void finishExam()} aria-describedby="finishExamInstructions">
+          {t('finish-exam.finish')}
+        </button>
+        <div className="e-finish-exam-instructions" id="finishExamInstructions">
+          {t('finish-exam.instructions')}
+        </div>
+      </div>
+    </SectionElement>
+  )
 }
 
 export default React.memo(ExamFooter)
