eff6b6b8862c5afe24df3afdb163a2121141249c
Heikki Mantere <heikki.mantere@gmail.com>
2023-09-28 12:01:37 +0300

    Add YTL footer to EE

--- packages/core/src/components/exam/Exam.tsx
+++ packages/core/src/components/exam/Exam.tsx
@@ -262,6 +262,7 @@ const Exam: React.FunctionComponent<ExamProps> = ({
               </div>
             </div>
           </div>
+          <div className="e-section e-exam-footer-content" />
           <div className="e-footer">
             <ErrorIndicator />
             <SaveIndicator />
