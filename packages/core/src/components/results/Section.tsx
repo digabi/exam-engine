ca3597bc695d384f45baca62c56b0efcf8e00caa
Heikki Mantere <heikki.mantere@gmail.com>
2023-10-09 12:04:07 +0300

    Fix paddings and margins

--- packages/core/src/components/results/Section.tsx
+++ packages/core/src/components/results/Section.tsx
@@ -1,24 +1,16 @@
 import React, { useContext } from 'react'
-import { query } from '../../dom-utils'
 import SectionElement from '../SectionElement'
 import { SectionContext, withSectionContext } from '../context/SectionContext'
-import { ResultsContext } from '../context/ResultsContext'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { sectionTitleId } from '../../ids'
 
 function Section({ element, renderChildNodes }: ExamComponentProps) {
   const { displayNumber } = useContext(SectionContext)
-  const { answersByQuestionId } = useContext(ResultsContext)
-  const containsAnswers = query(element, e => {
-    const maybeQuestionId = e.getAttribute('question-id')
-    return maybeQuestionId != null && Object.prototype.hasOwnProperty.call(answersByQuestionId, maybeQuestionId)
-  })
 
-  return containsAnswers ? (
+  return (
     <SectionElement aria-labelledby={sectionTitleId(displayNumber)}>
       <div className="e-results-section-wrapper">{renderChildNodes(element)}</div>
     </SectionElement>
-  ) : null
+  )
 }
-
 export default React.memo(withSectionContext(Section))
