0587faf9769c3d8b3ba7abab4cd08d3ae1449018
Heikki Mantere <heikki.mantere@gmail.com>
2023-10-09 12:04:07 +0300

    Fix paddings and margins

--- packages/core/src/components/results/Question.tsx
+++ packages/core/src/components/results/Question.tsx
@@ -14,7 +14,7 @@ function Question({ element, renderChildNodes }: ExamComponentProps) {
 
   return (
     <div
-      className={classNames('e-exam-question', {
+      className={classNames('e-exam-question', `e-level-${level}`, {
         'e-mrg-b-8 e-clearfix': level === 0,
         'e-mrg-l-8 e-mrg-y-4': level > 0,
         noAnswer: !hasAnswers
