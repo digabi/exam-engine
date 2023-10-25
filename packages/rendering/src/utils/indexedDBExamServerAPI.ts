18657738f2277626ec459d94283e36a129003327
Heikki Mantere <heikki.mantere@gmail.com>
2023-10-25 11:35:23 +0300

    Fix types

--- packages/rendering/src/utils/indexedDBExamServerAPI.ts
+++ packages/rendering/src/utils/indexedDBExamServerAPI.ts
@@ -36,7 +36,7 @@ export default function indexedDBExamServerAPI(
   const examServerApi: ExamServerAPI = {
     setCasStatus: casStatus => Promise.resolve(casStatus),
     getAnswers: () => db.answer.where({ examUuid }).toArray(),
-    finishExam: () => Promise.resolve(),
+    finishExam: () => undefined,
     saveAnswer: async answer => {
       await db.answer.put({ ...answer, examUuid })
     },
