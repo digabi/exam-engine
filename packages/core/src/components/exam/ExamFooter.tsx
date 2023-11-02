82f953007d1b45bba1e57f40bd2fb04fa241287b
Heikki Mantere <heikki.mantere@gmail.com>
2023-11-02 14:27:24 +0200

    Always render FinishExam button regardless of xml's structure

--- packages/core/src/components/exam/ExamFooter.tsx
+++ packages/core/src/components/exam/ExamFooter.tsx
@@ -1,32 +1,8 @@
-import React, { useContext } from 'react'
-import SectionElement from '../SectionElement'
+import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
-import { ExamContext } from '../context/ExamContext'
-import { useExamTranslation } from '../../i18n'
 
 function ExamFooter({ element, renderChildNodes }: ExamComponentProps) {
-  const { examServerApi } = useContext(ExamContext)
-  const { finishExam } = examServerApi
-  const { t } = useExamTranslation()
-
-  return (
-    <SectionElement>
-      <div className="e-exam-footer">
-        {renderChildNodes(element)}
-
-        {finishExam !== undefined && (
-          <>
-            <button id="finishExam" onClick={() => void finishExam()} aria-describedby="finishExamInstructions">
-              {t('finish-exam.finish')}
-            </button>
-            <div className="e-finish-exam-instructions" id="finishExamInstructions">
-              {t('finish-exam.instructions')}
-            </div>
-          </>
-        )}
-      </div>
-    </SectionElement>
-  )
+  return <div className="e-exam-footer">{renderChildNodes(element)}</div>
 }
 
 export default React.memo(ExamFooter)
