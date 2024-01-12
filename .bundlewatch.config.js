3d1d2f428224af2b83097f9c095326986328c1af
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-01-12 14:21:30 +0200

    Increase core bundle size again
    Double it's size as there is no hard limit fot it's size

--- .bundlewatch.config.js
+++ .bundlewatch.config.js
@@ -2,16 +2,16 @@ const currentBranch = require('ci-env').branch
 
 module.exports = {
   ci: {
-    trackBranches: [currentBranch],
+    trackBranches: [currentBranch]
   },
   files: [
     {
       path: 'packages/core/dist/main-bundle.js',
-      maxSize: '250KB',
+      maxSize: '500KB'
     },
     {
       path: 'packages/core/dist/main.css',
-      maxSize: '14KB',
-    },
-  ],
+      maxSize: '14KB'
+    }
+  ]
 }
