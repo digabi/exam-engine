4459e962c0dd20580ebedfb6175bfa2f04d443f3
Heikki Mantere <heikki.mantere@gmail.com>
2024-04-15 10:59:12 +0300

    Add classname

--- packages/core/src/components/grading/GradingAnswer.tsx
+++ packages/core/src/components/grading/GradingAnswer.tsx
@@ -124,7 +124,7 @@ function GradingAnswerWithTranslations({
   return (
     <div onClick={e => onAnnotationOrListClick(e)} className="e-grading-answer-wrapper">
       {loadedCount !== totalImages && (
-        <div>
+        <div className="loading-images-counter">
           Ladataan kuvia... ({loadedCount}/{totalImages})
         </div>
       )}
