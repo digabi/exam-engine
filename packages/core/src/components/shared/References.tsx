e3eb1fe3912d37a7d8a91761e8ac652fbfe10a61
Heikki Mantere <heikki.mantere@gmail.com>
2024-05-08 15:19:19 +0300

    Add data-annotation-anchors to instructions and references

--- packages/core/src/components/shared/References.tsx
+++ packages/core/src/components/shared/References.tsx
@@ -19,7 +19,7 @@ function References(_props: ExamComponentProps) {
   return internalReferences.length > 0 ? (
     <SectionElement aria-labelledby={referencesTitleId}>
       <h2 id={referencesTitleId}>{t('references.heading')}</h2>
-      <ol className="e-list-data">
+      <ol className="e-list-data" data-annotation-anchor={referencesTitleId}>
         {internalReferences.map((reference, i) => {
           const question = queryAncestors(reference, 'question')!
           const displayNumber = question.getAttribute('display-number')!
