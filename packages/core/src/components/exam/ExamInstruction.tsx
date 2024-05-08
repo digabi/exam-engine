e89711de2f7cb2e154bc79ae4396f6ec8b69db83
Heikki Mantere <heikki.mantere@gmail.com>
2024-05-08 15:19:19 +0300

    Add data-annotation-anchors to instructions and references

--- packages/core/src/components/exam/ExamInstruction.tsx
+++ packages/core/src/components/exam/ExamInstruction.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 
 function ExamInstruction({ element, renderChildNodes }: ExamComponentProps) {
-  return <div className="exam-instruction notification e-pad-4">{renderChildNodes(element)}</div>
+  return (
+    <div className="exam-instruction notification e-pad-4" data-annotation-anchor="exam-instruction">
+      {renderChildNodes(element)}
+    </div>
+  )
 }
 
 export default React.memo(ExamInstruction)
--- packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 
 const ExamGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => (
-  <div className="e-exam-grading-instruction notification e-pad-4">{renderChildNodes(element)}</div>
+  <div className="e-exam-grading-instruction notification e-pad-4" data-annotation-anchor="exam-grading-instruction">
+    {renderChildNodes(element)}
+  </div>
 )
 
 export default ExamGradingInstruction
