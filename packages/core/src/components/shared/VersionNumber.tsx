619788751223ebeacbefb340bcb4d0034cb42855
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-05-15 06:54:23 +0300

    Version release changes version number and that requires snapshot update.
     Set default version number for tests so snapshots does not have to be updated after version release.

--- packages/core/src/components/shared/VersionNumber.tsx
+++ packages/core/src/components/shared/VersionNumber.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import * as _ from 'lodash-es'
 import lernaConfig from '../../../../../lerna.json'
 
-export const VersionNumber: React.FC = () => <span>EE v.{lernaConfig.version}</span>
+export const VersionNumber: React.FC = () => {
+  const testVersion = _.get(window, 'TEST_VERSION_NUMBER')
+  return <span>EE v.{testVersion || lernaConfig.version}</span>
+}
