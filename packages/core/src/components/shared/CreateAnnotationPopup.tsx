52d15d7bb2ee9378e1fab385ee2ef879c52281f6
Heikki Mantere <heikki.mantere@gmail.com>
2024-05-24 14:03:56 +0300

    Position popup correctly

--- packages/core/src/components/shared/CreateAnnotationPopup.tsx
+++ packages/core/src/components/shared/CreateAnnotationPopup.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function CreateAnnotationPopup({
   updateComment,
-  closeEditor
+  closeEditor,
+  markRef
 }: {
   updateComment: (value: string) => void
   closeEditor: () => void
+  markRef: React.RefObject<HTMLElement>
 }) {
   const [comment, setComment] = useState<string>('')
 
+  const popupRef = React.createRef<HTMLElement>()
+
+  useEffect(() => {
+    showAndPositionElement(markRef, popupRef)
+  }, [])
+
   return (
-    <span className="annotation-popup" style={{ position: 'absolute' }}>
+    <span className="annotation-popup" style={{ position: 'absolute' }} ref={popupRef}>
       <textarea
         className="comment-content"
         role="textbox"
@@ -38,4 +46,23 @@ export function CreateAnnotationPopup({
       </span>
     </span>
   )
+}
+
+function showAndPositionElement(markRef: React.RefObject<HTMLElement>, popupRef: React.RefObject<HTMLElement>) {
+  const mark = markRef.current
+  const popup = popupRef.current
+  if (mark && popup) {
+    const style = popup.style
+    const markRect = mark?.getBoundingClientRect()
+    const popupRect = popup.getBoundingClientRect()
+    const top = markRect.height
+    const windowWidth = window.innerWidth
+    const popupHitsWindowRight = popupRect.right > windowWidth
+    style.top = `${String(top)}px`
+    if (popupHitsWindowRight) {
+      style.right = '0'
+    } else {
+      style.left = '0'
+    }
+  }
 }
