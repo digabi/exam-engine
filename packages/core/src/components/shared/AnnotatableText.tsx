b937a31af07f1c7e7efd1a525e820b727154c082
Heikki Mantere <heikki.mantere@gmail.com>
2024-05-31 15:43:33 +0300

    If mark is hidden, render marked text but not inside mark

--- packages/core/src/components/shared/AnnotatableText.tsx
+++ packages/core/src/components/shared/AnnotatableText.tsx
@@ -67,12 +67,15 @@ export const AnnotatableText = ({
 
       nodes.push(
         annotation.hidden ? (
-          <mark
-            key={annotation.startIndex}
-            className="e-annotation"
-            data-annotation-id={isExamAnnotation(annotation) ? annotation.annotationId : ''}
-            data-hidden="true"
-          />
+          <>
+            <mark
+              key={annotation.startIndex}
+              className="e-annotation"
+              data-annotation-id={isExamAnnotation(annotation) ? annotation.annotationId : ''}
+              data-hidden="true"
+            />
+            {markedText}
+          </>
         ) : (
           <Mark
             key={annotation.startIndex}
