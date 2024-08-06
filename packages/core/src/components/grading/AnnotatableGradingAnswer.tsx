1e9f3eaf1046d766481bcb92e0fbd2747da78c0e
Heikki Mantere <heikki.mantere@gmail.com>
2024-08-06 11:04:35 +0300

    Return multiple annotations sections from one selection

---
+++ packages/core/src/components/grading/AnnotatableGradingAnswer.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { AnnotationContext } from '../context/AnnotationProvider'
+import { markText } from '../shared/AnnotatableText'
+import { GradingAnswerProps } from './GradingAnswer'
+
+export function AnnotatableGradingAnswer(props: GradingAnswerProps) {
+  const path = 'pregrading'
+  const textWithoutLineBreaksAndExtraSpaces = props.answer.value
+  const annotationContextData = useContext(AnnotationContext)
+  const { newAnnotation, annotations, onClickAnnotation, setNewAnnotationRef } = annotationContextData
+
+  const canNotBeAnnotated =
+    annotationContextData?.annotations === undefined || textWithoutLineBreaksAndExtraSpaces.length === 0
+  if (canNotBeAnnotated) {
+    return textWithoutLineBreaksAndExtraSpaces
+  }
+  const newAnnotationForThisNode = newAnnotation?.annotationAnchor === path ? newAnnotation : null
+
+  const thisNodeAnnotations = [
+    ...(annotations?.[path] || []),
+    ...(newAnnotationForThisNode ? [newAnnotationForThisNode] : [])
+  ]
+
+  console.log(thisNodeAnnotations)
+
+  return (
+    <span className="e-annotatable" data-annotation-path={path} data-testid="123">
+      {thisNodeAnnotations?.length > 0 && onClickAnnotation
+        ? markText(textWithoutLineBreaksAndExtraSpaces, thisNodeAnnotations, onClickAnnotation, setNewAnnotationRef)
+        : textWithoutLineBreaksAndExtraSpaces}
+    </span>
+  )
+}
