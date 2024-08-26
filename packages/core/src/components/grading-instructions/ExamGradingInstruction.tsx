c91a4fecc1bdaacd754395183e76490457809d61
Petri Ämmälä <petri.ammala@reaktor.com>
2024-08-26 16:55:52 +0300

    Add path for editable grading instructions to make updating exam xml possible

--- packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { GradingInstructionContext } from '../context/GradingInstructionContext'
-import { EditableGradingInstruction } from './EditableGradingInstruction'
+import EditableGradingInstruction from './EditableGradingInstruction'
 
 const ExamGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
   const { editable } = useContext(GradingInstructionContext)
 
   return (
     <div className="e-exam-grading-instruction notification e-pad-4" data-annotation-anchor="exam-grading-instruction">
-      {editable ? <EditableGradingInstruction element={element} /> : renderChildNodes(element)}
+      {editable ? (
+        <EditableGradingInstruction element={element} renderChildNodes={renderChildNodes} />
+      ) : (
+        renderChildNodes(element)
+      )}
     </div>
   )
 }
