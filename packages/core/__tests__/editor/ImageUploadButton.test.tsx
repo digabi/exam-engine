6a71364e03ae6f70f1e154d75b2e9b3c4d827ab0
Heini <ahokas.heini@gmail.com>
2024-09-25 17:05:44 +0300

    test: add more component tests for imageuplodabutton

--- packages/core/__tests__/editor/ImageUploadButton.test.tsx
+++ packages/core/__tests__/editor/ImageUploadButton.test.tsx
@@ -1,12 +1,22 @@
 import '@testing-library/jest-dom'
-import { cleanup, act as testAct, fireEvent } from '@testing-library/react'
-import { mockCreateRange, promisifiedFireEvent } from '../utils/prosemirror'
-import { renderGradingInstruction } from '../utils/renderEditableGradingInstruction'
+import { cleanup, act as testAct, waitFor } from '@testing-library/react'
+import { mockCreateRange } from '../utils/prosemirror'
+import {
+  renderGradingInstruction,
+  mockedPermanentUrl,
+  mockedResolvedPath
+} from '../utils/renderEditableGradingInstruction'
+import userEvent from '@testing-library/user-event'
+import { insertText } from '../utils/util'
+import { EditorView } from 'prosemirror-view'
+
+jest.spyOn(EditorView.prototype, 'focus').mockImplementation(() => {})
 
 const act = testAct as (func: () => Promise<void>) => Promise<void>
 
 describe('ImageUploadButton', () => {
   let onContentChangeMock: jest.Mock
+  const mockedTempUrl = 'mocked-temp-url'
 
   beforeEach(() => {
     onContentChangeMock = jest.fn()
@@ -14,6 +24,8 @@ describe('ImageUploadButton', () => {
 
   beforeAll(() => {
     mockCreateRange()
+    global.URL.createObjectURL = jest.fn(() => mockedTempUrl)
+    File.prototype.arrayBuffer = jest.fn().mockResolvedValue(new ArrayBuffer(8))
   })
 
   afterEach(() => {
@@ -21,24 +33,56 @@ describe('ImageUploadButton', () => {
   })
 
   describe('Renders Image', () => {
+    const htmlAttrs = {
+      width: '504',
+      height: '504',
+      class: 'foo',
+      lang: 'fi-FI',
+      src: `/${mockedResolvedPath}/foo.bar`
+    }
+
+    const xmlAttrs = {
+      lang: htmlAttrs.lang,
+      class: htmlAttrs.class,
+      src: 'foo.bar'
+    }
+
     it('e:image tags are rendered in and rendered out with correct attributes and tags', async () => {
-      const { getByRole, getByText } = renderGradingInstruction(
-        '<div><p>hello</p><e:image data-editor-id="e-image" width="954" alt="foo" src="foo.bar"></e:image></div>',
+      const container = renderGradingInstruction(
+        `<div><p>hello</p><e:image data-editor-id="e-image" width=${htmlAttrs.width} height=${htmlAttrs.height} lang=${htmlAttrs.lang} class=${htmlAttrs.class} src=${xmlAttrs.src}></e:image></div>`,
         onContentChangeMock
       )
-      const image = getByRole('img', { name: 'foo' })
-      const text = getByText('hello')
-      expect(image).toHaveAttribute('src', 'foo.bar')
-      expect(image).toHaveAttribute('width', '954')
-      await act(
-        async () =>
-          await promisifiedFireEvent(() => fireEvent.input(text, { target: { innerText: 'text', innerHTML: 'text' } }))
-      )
+      const image = container.getByRole('img')
+      for (const [key, value] of Object.entries(htmlAttrs)) {
+        expect(image).toHaveAttribute(key, value)
+      }
+      await act(async () => {
+        insertText(await container.findByText('hello'), 'hello world')
+      })
 
       expect(onContentChangeMock).toHaveBeenCalledTimes(1)
-      expect(onContentChangeMock).toHaveBeenCalledWith('<p>text</p><p><e:image src="foo.bar"></e:image></p>', '')
+      expect(onContentChangeMock).toHaveBeenCalledWith(
+        `<p>hello world</p><p><e:image lang="${xmlAttrs.lang}" class="${xmlAttrs.class}" src="${xmlAttrs.src}"></e:image></p>`,
+        ''
+      )
     })
 
-    it.skip('image can be uploaded', () => {})
+    it('image can be uploaded', async () => {
+      const container = renderGradingInstruction('', onContentChangeMock)
+      const file = new File(['hello'], 'hello.png', { type: 'image/png' })
+      const input = container.queryByTestId('image-upload-button')
+      await userEvent.upload(input!, file)
+      await waitFor(() => {
+        expect(onContentChangeMock).toHaveBeenCalledTimes(2)
+        expect(onContentChangeMock).toHaveBeenNthCalledWith(1, `<p><e:image src="${mockedTempUrl}"></e:image></p>`, '')
+        expect(onContentChangeMock).toHaveBeenNthCalledWith(
+          2,
+          `<p><e:image src="${mockedPermanentUrl}"></e:image></p>`,
+          ''
+        )
+        const image = container.getByRole('img')
+        expect(image).toHaveAttribute('src', mockedPermanentUrl)
+      })
+    })
   })
 })
