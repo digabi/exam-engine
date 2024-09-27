6b173dd92f1f016baad3fe9b7cf26174a79619ef
Petri Ämmälä <petri.ammala@reaktor.com>
2024-09-27 08:23:15 +0300

    Revert exam grading instruction being editable
    Only need to edit answer grading instructions.

--- packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/ExamGradingInstruction.tsx
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
-import { GradingInstructionContext } from '../context/GradingInstructionContext'
-import EditableGradingInstruction from './EditableGradingInstruction'
 
-const ExamGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
-  const { editable } = useContext(GradingInstructionContext)
+const ExamGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => (
+  <div className="e-exam-grading-instruction notification e-pad-4" data-annotation-anchor="exam-grading-instruction">
+    {renderChildNodes(element)}
+  </div>
+)
 
-  return (
-    <div className="e-exam-grading-instruction notification e-pad-4" data-annotation-anchor="exam-grading-instruction">
-      {editable ? <EditableGradingInstruction element={element} /> : renderChildNodes(element)}
-    </div>
-  )
-}
 export default ExamGradingInstruction
