10477221c089472332c07926167d74db79fcd0c9
Petri Ämmälä <petri.ammala@reaktor.com>
2024-10-01 14:23:58 +0300

    Show language and exam-type target of the selected element

--- packages/core/src/components/grading-instructions/EditableGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/EditableGradingInstruction.tsx
@@ -4,7 +4,7 @@ import { EditorState } from 'prosemirror-state'
 import { baseKeymap } from 'prosemirror-commands'
 import { GradingInstructionContext } from '../context/GradingInstructionContext'
 import { ProseMirror } from '@nytimes/react-prosemirror'
-import { DOMParser as ProseDOMParser, DOMSerializer, Schema } from 'prosemirror-model'
+import { DOMParser as ProseDOMParser, DOMSerializer, Schema, Node } from 'prosemirror-model'
 import { keymap } from 'prosemirror-keymap'
 import { FormulaButton, FormulaEditorState, formulaOutputSchema, FormulaPlugin, formulaSchema } from './editor/Formula'
 import { TableMenu, tableSchema } from './editor/Table'
@@ -13,6 +13,7 @@ import FormatButton from './editor/FormatButton'
 import { NbspButton, nbspPlugin } from './editor/NBSP'
 import { spanWithNowrap } from './editor/spanWithNowrap'
 import { localization } from './editor/localization'
+import { CurrentLocalization } from './editor/CurrentLocalization'
 
 const schema = new Schema({
   nodes: baseSchema.spec.nodes.append(formulaSchema).append(tableSchema),
@@ -24,15 +25,22 @@ const outputSchema = new Schema({
   marks: baseSchema.spec.marks.append(spanWithNowrap).append(localization(false))
 })
 
-function Menu(props: { setFormulaState: (values: FormulaEditorState) => void }) {
+function Menu({
+  setFormulaState,
+  currentElement
+}: {
+  setFormulaState: (values: FormulaEditorState) => void
+  currentElement: Element | null
+}) {
   return (
-    <>
+    <div className="ProseMirror-menu">
       <FormatButton markName="strong" displayName="Bold" />
       <FormatButton markName="em" displayName="Italic" />
       <TableMenu />
-      <FormulaButton setFormulaState={props.setFormulaState} />
+      <FormulaButton setFormulaState={setFormulaState} />
       <NbspButton />
-    </>
+      <CurrentLocalization currentElement={currentElement} />
+    </div>
   )
 }
 
@@ -45,12 +53,31 @@ function EditableGradingInstruction({ element }: { element: Element }) {
   const [state, setState] = useState(
     EditorState.create({ schema, doc, plugins: [keymap(baseKeymap), formulaPlugin, nbspPlugin] })
   )
+  const [currentElement, setCurrentElement] = useState<Element | null>(null)
+
+  function findElementFromPosition(rootNode: Node | null, pos: number) {
+    if (rootNode == null) {
+      return null
+    }
+    const node = rootNode.nodeAt(pos)
+    if (!node) {
+      return null
+    }
+    const serializedNode = DOMSerializer.fromSchema(outputSchema).serializeNode(node)
+    const element = serializedNode as Element
+    if (element.tagName?.toLowerCase() == 'e:localization') {
+      return element
+    }
+    const parent = serializedNode.parentNode as Node | null
+    return findElementFromPosition(parent, pos)
+  }
 
   return (
     <ProseMirror
       mount={mount}
       state={state}
       dispatchTransaction={tr => {
+        setCurrentElement(findElementFromPosition(tr.doc, tr.selection.from))
         setState(s => {
           const newContent = s.apply(tr)
           if (tr.docChanged) {
@@ -70,7 +97,7 @@ function EditableGradingInstruction({ element }: { element: Element }) {
         })
       }}
     >
-      <Menu setFormulaState={setFormulaState} />
+      <Menu setFormulaState={setFormulaState} currentElement={currentElement} />
       <div ref={setMount} />
 
       {formulaState && (
