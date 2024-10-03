d3fd7bf86490c71bdfb65476bc5f66bbe332baf9
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-10-03 15:58:03 +0300

    Place all schemas in schema folder

--- packages/core/src/components/grading-instructions/editor/Formula.tsx
+++ packages/core/src/components/grading-instructions/editor/Formula.tsx
@@ -1,66 +1,10 @@
 import { Plugin, PluginKey, Transaction } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
-import { AttributeSpec, Fragment, Node, NodeSpec } from 'prosemirror-model'
+import { Fragment } from 'prosemirror-model'
 import { useEditorEventCallback } from '@nytimes/react-prosemirror'
 import React from 'react'
 
 export type FormulaEditorState = { pos: number; latex: string; element: Element }
-
-export const formulaSchema: NodeSpec = {
-  formula: {
-    inline: true,
-    group: 'inline',
-    atom: true,
-    attrs: {
-      latex: {},
-      mode: { default: '' },
-      assistiveTitle: { default: '' }
-    },
-    parseDOM: [
-      {
-        tag: '[data-editor-id=e-formula]',
-        getAttrs(dom: HTMLElement) {
-          return {
-            latex: dom.textContent,
-            mode: dom.getAttribute('mode'),
-            assistiveTitle: dom.getAttribute('assistive-title')
-          }
-        }
-      }
-    ],
-    toDOM(node: Node) {
-      const container = document.createElement('img')
-      if (node.attrs.latex) {
-        container.setAttribute('alt', node.attrs.latex as string)
-        container.setAttribute('formula', 'true')
-        container.setAttribute('src', `/math.svg?latex=${encodeURIComponent(node.attrs.latex as string)}`)
-      }
-      return container
-    }
-  }
-}
-
-export const formulaOutputSchema: NodeSpec = {
-  formula: {
-    ...(formulaSchema.formula as AttributeSpec),
-    toDOM(node: Node) {
-      if (!node.attrs.latex) {
-        return ''
-      }
-      const formulaElement = document.createElement('e:formula')
-      if (node.attrs.mode) {
-        formulaElement.setAttribute('mode', node.attrs.mode as string)
-      }
-      if (node.attrs.assistiveTitle) {
-        formulaElement.setAttribute('assistive-title', node.attrs.assistiveTitle as string)
-      }
-      formulaElement.textContent = node.attrs.latex as string
-      const container = document.createElement('span')
-      container.appendChild(formulaElement)
-      return container.firstElementChild!
-    }
-  }
-}
 
 export function FormulaButton(props: { disabled: boolean; setFormulaState: (values: FormulaEditorState) => void }) {
   const onClick = useEditorEventCallback(view => {
