4b7a4bce7edca1fce43ad1197a58c29cd336d70b
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-10-09 08:16:47 +0300

    nop

--- packages/core/src/components/grading-instructions/AnswerGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/AnswerGradingInstruction.tsx
@@ -4,6 +4,7 @@ import { GradingInstructionContext } from '../context/GradingInstructionContext'
 
 const AnswerGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
   const { EditorComponent } = useContext(GradingInstructionContext)
+
   return (
     <div className="e-answer-grading-instruction e-multiline-results-text-answer e-mrg-b-2 e-pad-l-2">
       {EditorComponent ? <EditorComponent element={element} /> : renderChildNodes(element)}
