f23912259c8e32dcb2cf38cbf3ccb079851186f8
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-10-09 07:36:41 +0300

    Move prosemirror editor to exam-maker

--- packages/core/src/components/grading-instructions/AnswerGradingInstruction.tsx
+++ packages/core/src/components/grading-instructions/AnswerGradingInstruction.tsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { GradingInstructionContext } from '../context/GradingInstructionContext'
-import EditableGradingInstruction from './EditableGradingInstruction'
 
 const AnswerGradingInstruction: React.FunctionComponent<ExamComponentProps> = ({ element, renderChildNodes }) => {
-  const { editable } = useContext(GradingInstructionContext)
+  const { EditorComponent } = useContext(GradingInstructionContext)
+
   return (
     <div className="e-answer-grading-instruction e-multiline-results-text-answer e-mrg-b-2 e-pad-l-2">
-      {editable ? <EditableGradingInstruction element={element} /> : renderChildNodes(element)}
+      {EditorComponent ? <EditorComponent element={element} /> : renderChildNodes(element)}
     </div>
   )
 }
