f9bbf2b3f85dce065f0794b8d547feb9856017ac
antti-manninen-vmv <antti.manninen@voidmainvoid.fi>
2024-10-17 10:25:21 +0300

    Extract common popup component that can be used elsewhere

--- packages/core/__tests__/annotations.test.tsx
+++ packages/core/__tests__/annotations.test.tsx
@@ -62,7 +62,7 @@ describe('Annotations', () => {
       <Exam {...getExamProps()} annotations={[]} onClickAnnotation={() => {}} onSaveAnnotation={() => {}} />
     )
     await annotateText(exam, defaultAnnotationAnchor)
-    expect(exam.getByTestId('annotation-popup')).toBeVisible()
+    expect(exam.getByTestId('e-popup')).toBeVisible()
   })
 
   it('popup is not rendered if annotation props are not passed', async () => {
@@ -71,7 +71,7 @@ describe('Annotations', () => {
     const textElement = exam.getByText(text)
     mockWindowSelection(text, textElement, text.length)
     await userEvent.click(textElement)
-    expect(exam.queryByTestId('annotation-popup')).not.toBeInTheDocument()
+    expect(exam.queryByTestId('e-popup')).not.toBeInTheDocument()
   })
 
   it('callback is called when annotation is saved', async () => {
@@ -81,7 +81,7 @@ describe('Annotations', () => {
     )
     await annotateText(exam, defaultAnnotationAnchor)
 
-    const textbox = exam.getByTestId('edit-comment')
+    const textbox = exam.getByTestId('e-popup-content')
     fireEvent.input(textbox, {
       target: { innerText: 'New comment', innerHTML: 'New comment' }
     })
@@ -112,14 +112,14 @@ describe('Annotations', () => {
       <Exam {...getExamProps()} annotations={[]} onClickAnnotation={() => {}} onSaveAnnotation={() => {}} />
     )
     await annotateText(exam, defaultAnnotationAnchor)
-    const textbox = exam.getByTestId('edit-comment')
+    const textbox = exam.getByTestId('e-popup-content')
     fireEvent.input(textbox, {
       target: { innerText: 'New Value', innerHTML: 'New Value' }
     })
     await userEvent.click(exam.getByText('Tallenna'))
     // text picked by getMarkedText includes leading/trailing whitespace, so they must be here too
     await annotateText(exam, annotationAnchor_7_1)
-    expect(exam.getByTestId('edit-comment').textContent).toHaveLength(0)
+    expect(exam.getByTestId('e-popup-content').textContent).toHaveLength(0)
   })
 
   it('callback is called when annotation is clicked', async () => {
@@ -266,7 +266,7 @@ describe('Annotations', () => {
       />
     )
     await annotateText(gi, defaultAnnotationAnchor)
-    expect(gi.getByTestId('annotation-popup')).toBeVisible()
+    expect(gi.getByTestId('e-popup')).toBeVisible()
   })
 
   it('attachments can be annotated', async () => {
@@ -276,7 +276,7 @@ describe('Annotations', () => {
       <Attachments {...getCommonProps()} annotations={[]} onClickAnnotation={() => {}} onSaveAnnotation={() => {}} />
     )
     await annotateText(gi, annotationAnchor_7_A)
-    expect(gi.getByTestId('annotation-popup')).toBeVisible()
+    expect(gi.getByTestId('e-popup')).toBeVisible()
   })
 
   function createAnnotations(annotations: Annotation[]): ExamAnnotation[] {
