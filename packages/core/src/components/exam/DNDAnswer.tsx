392bc7c9aa89f9022e3bf6f8ce87c1c562f805ec
Heikki Mantere <heikki.mantere@gmail.com>
2024-11-15 12:24:07 +0200

    Use common components for dnd-answer in exam and results contexts

--- packages/core/src/components/exam/DNDAnswer.tsx
+++ packages/core/src/components/exam/DNDAnswer.tsx
@@ -1,73 +1,13 @@
 import { UniqueIdentifier, useDroppable } from '@dnd-kit/core'
 import { SortableContext } from '@dnd-kit/sortable'
-import classNames from 'classnames'
 import React from 'react'
 import { ExamComponentProps } from '../..'
-import { getNumericAttribute, query } from '../../dom-utils'
+import { DNDAnswerCommon } from '../shared/DNDAnswerCommon'
 import { Score } from '../shared/Score'
-import { DNDAnswerOption } from './DNDAnswerOption'
 
-type ItemsState = {
+export type ItemsState = {
   root: UniqueIdentifier[]
   [key: UniqueIdentifier]: UniqueIdentifier[]
-}
-
-export const DNDTitleAndAnswer = ({
-  element,
-  renderChildNodes,
-  items,
-  answerOptionsByQuestionId
-}: {
-  element: Element
-  renderChildNodes: ExamComponentProps['renderChildNodes']
-  items: ItemsState
-  answerOptionsByQuestionId: Record<UniqueIdentifier, Element>
-}) => {
-  const titleElement = query(element, 'dnd-answer-title')
-  const maxScore = getNumericAttribute(element, 'max-score')
-  const questionId = element.getAttribute('question-id')!
-  const displayNumber = element.getAttribute('display-number')!
-
-  return (
-    <div
-      className={classNames('e-dnd-answer', {
-        root: questionId === 'root'
-      })}
-      data-question-id={questionId}
-      key={questionId}
-    >
-      {titleElement && <DNDAnswerTitle element={titleElement} renderChildNodes={renderChildNodes} />}
-      <div className="connection-line" />
-
-      <DNDAnswer
-        titleElement={titleElement}
-        renderChildNodes={renderChildNodes}
-        items={items}
-        answerOptionsByQuestionId={answerOptionsByQuestionId}
-        questionId={questionId}
-        displayNumber={displayNumber}
-      />
-
-      {maxScore ? <Score score={maxScore} size="small" /> : null}
-    </div>
-  )
-}
-
-const DNDAnswerTitle = ({ element, renderChildNodes }: ExamComponentProps) => {
-  const hasImages = !!query(element, 'image')
-
-  if (!renderChildNodes(element).length) {
-    return <i style={{ color: 'grey' }}>Tähän tulee kysymys...</i>
-  } else
-    return (
-      <span
-        className={classNames('e-dnd-answer-title', {
-          'has-images': hasImages
-        })}
-      >
-        {renderChildNodes(element)}
-      </span>
-    )
 }
 
 export const DNDAnswer = ({
@@ -75,7 +15,8 @@ export const DNDAnswer = ({
   items,
   answerOptionsByQuestionId,
   questionId,
-  displayNumber
+  displayNumber,
+  maxScore
 }: {
   items: ItemsState
   answerOptionsByQuestionId: Record<UniqueIdentifier, Element>
@@ -83,36 +24,30 @@ export const DNDAnswer = ({
   renderChildNodes: ExamComponentProps['renderChildNodes']
   titleElement?: Element
   displayNumber?: string
+  maxScore?: number
 }) => {
   const idsInGroup = items[questionId] || []
-  const dndAnswerOptions = idsInGroup.map(id => answerOptionsByQuestionId[id])
   const { setNodeRef, isOver, active } = useDroppable({ id: questionId })
-
-  const hasImages = dndAnswerOptions.some(option => query(option, 'image'))
-  const hasAudio = dndAnswerOptions.some(option => query(option, 'audio'))
-  const hasFormula = dndAnswerOptions.some(option => query(option, 'formula'))
 
   return (
     <SortableContext id={String(questionId)} items={idsInGroup}>
-      <div className="anchor" id={`question-nr-${displayNumber}`} />
-      <div
-        ref={setNodeRef}
-        className={classNames('e-dnd-answer-droppable', {
-          hovered: isOver,
-          'ready-for-drop': !!active?.id,
-          'has-images': hasImages,
-          'has-audio': hasAudio,
-          'has-formula': hasFormula,
-          root: questionId === 'root'
-        })}
-      >
-        {dndAnswerOptions?.map(element => {
-          const optionId = element.getAttribute('option-id')!
-          return (
-            <DNDAnswerOption element={element} renderChildNodes={renderChildNodes} key={optionId} value={optionId} />
-          )
-        })}
+      <div ref={setNodeRef}>
+        <DNDAnswerCommon
+          renderChildNodes={renderChildNodes}
+          items={items}
+          answerOptionsByQuestionId={answerOptionsByQuestionId}
+          questionId={questionId}
+          displayNumber={displayNumber}
+          classes={{
+            hovered: isOver,
+            root: questionId === 'root',
+            'ready-for-drop': !!active?.id
+          }}
+          isInExam={true}
+        />
       </div>
+
+      {maxScore ? <Score score={maxScore} size="small" /> : null}
     </SortableContext>
   )
 }
---
+++ packages/core/src/components/shared/DNDTitleAndAnswerCommon.tsx
@@ -0,0 +1,69 @@
+import { UniqueIdentifier } from '@dnd-kit/core'
+import classNames from 'classnames'
+import React from 'react'
+import { ExamComponentProps } from '../..'
+import { getNumericAttribute, query } from '../../dom-utils'
+import { DNDAnswer as DNDAnswerExam, ItemsState } from '../exam/DNDAnswer'
+import { DNDAnswer as DNDAnswerResults } from '../results/DNDAnswer'
+
+export const DNDTitleAndAnswerCommon = ({
+  element,
+  renderChildNodes,
+  items,
+  answerOptionsByQuestionId,
+  isInExam
+}: {
+  element: Element
+  renderChildNodes: ExamComponentProps['renderChildNodes']
+  items: ItemsState
+  answerOptionsByQuestionId: Record<UniqueIdentifier, Element>
+  isInExam?: boolean
+}) => {
+  const questionId = element.getAttribute('question-id')!
+  const displayNumber = element.getAttribute('display-number')!
+  const maxScore = getNumericAttribute(element, 'max-score')
+  const hasImages = !!query(element, 'image')
+  const titleElement = query(element, 'dnd-answer-title')
+  const hasTitle = titleElement && renderChildNodes(titleElement).length > 0
+
+  return (
+    <div
+      className={classNames('e-dnd-answer', {
+        root: questionId === 'root'
+      })}
+      data-question-id={questionId}
+      key={questionId}
+    >
+      {titleElement &&
+        (!hasTitle ? (
+          <i style={{ color: 'grey' }}>Tästä puuttuu kysymys...</i>
+        ) : (
+          <span className={classNames('e-dnd-answer-title', { 'has-images': hasImages })}>
+            {renderChildNodes(titleElement)}
+          </span>
+        ))}
+
+      <div className="connection-line" />
+
+      {isInExam ? (
+        <DNDAnswerExam
+          renderChildNodes={renderChildNodes}
+          items={items}
+          answerOptionsByQuestionId={answerOptionsByQuestionId}
+          questionId={questionId}
+          displayNumber={displayNumber}
+          maxScore={maxScore}
+        />
+      ) : (
+        <DNDAnswerResults
+          renderChildNodes={renderChildNodes}
+          items={items}
+          answerOptionsByQuestionId={answerOptionsByQuestionId}
+          questionId={questionId}
+          displayNumber={displayNumber}
+          maxScore={maxScore}
+        />
+      )}
+    </div>
+  )
+}
