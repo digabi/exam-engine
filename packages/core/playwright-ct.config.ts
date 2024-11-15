4df0e2a58c197825568af33071b0145e6848e4c1
Heini <ahokas.heini@gmail.com>
2024-11-15 10:35:22 +0200

    test:run component tests on github actions

--- packages/core/playwright-ct.config.ts
+++ packages/core/playwright-ct.config.ts
@@ -7,14 +7,10 @@ export default defineConfig({
   testDir: './__tests__/playwright',
   timeout: 10 * 1000,
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'list',
   use: {
     trace: 'on-first-retry',
     ctPort: 3100,
