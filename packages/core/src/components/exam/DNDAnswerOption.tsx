185bac251dbfa3fd7dc67412fb0282b7742dab98
Heikki Mantere <heikki.mantere@gmail.com>
2024-11-18 15:18:03 +0200

    Fix image layout in dnd-option

--- packages/core/src/components/exam/DNDAnswerOption.tsx
+++ packages/core/src/components/exam/DNDAnswerOption.tsx
@@ -16,7 +16,7 @@ export const DNDAnswerOption = ({ element, renderChildNodes }: ExamComponentProp
   const style = { opacity: isDragging ? 0.3 : 1 }
 
   return (
-    <div ref={setNodeRef} className="e-dnd-answer-option">
+    <div ref={setNodeRef} style={{ display: 'flex' }}>
       <DNDAnswerOptionCommon element={element} renderChildNodes={renderChildNodes} style={style} />
 
       <div {...listeners} {...attributes} ref={setActivatorNodeRef} className="drag-handle">
