5daf0feff160a518f396a043fbc5123f021b1be0
Heini <ahokas.heini@gmail.com>
2024-11-26 12:12:33 +0200

    tests(core): use example exam for dnd component testing

--- packages/core/__tests__/playwright/DNDAnswerContainer.test.tsx
+++ packages/core/__tests__/playwright/DNDAnswerContainer.test.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { test, expect } from '@playwright/experimental-ct-react'
 import { readFileSync } from 'fs'
-import { resolve } from 'path'
+import path from 'path'
+import { test, expect } from '@playwright/experimental-ct-react'
+import { resolveExam } from '@digabi/exam-engine-exams'
+import { getMediaMetadataFromLocalFile, masterExam } from '@digabi/exam-engine-mastering'
 import { DNDAnswerContainerStory } from './stories/DNDAnswerContainer.story'
 
 test.describe('DNDAnswerContainer', () => {
   test('renders DNDAnswerContainer correctly', async ({ mount }) => {
-    const content = readFileSync(resolve(__dirname, './fixtures/basic-dnd-exam.xml'), 'utf-8')
-    const xml = readFileSync(resolve(__dirname, './fixtures/basic-dnd-component.xml'), 'utf-8')
-
-    const component = await mount(<DNDAnswerContainerStory content={xml} exam={content} />)
+    const examPath = resolveExam('SC/SC.xml')
+    const resolveAttachment = (filename: string) => path.resolve(path.dirname(examPath), 'attachments', filename)
+    const source = readFileSync(examPath, 'utf-8')
+    const results = await masterExam(source, () => '', getMediaMetadataFromLocalFile(resolveAttachment), {
+      removeCorrectAnswers: false
+    })
+    const component = await mount(<DNDAnswerContainerStory masteredExam={results} exam={source} />)
 
     // Add your assertions here
     await expect(component).toBeVisible()
--- packages/core/__tests__/playwright/stories/DNDAnswerContainer.story.tsx
+++ packages/core/__tests__/playwright/stories/DNDAnswerContainer.story.tsx
@@ -1,16 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
+import { MasteringResult } from '@digabi/exam-engine-mastering'
 import { renderChildNodes } from '../../../src/components/exam/Exam'
 import { DNDAnswerContainer } from '../../../src/components/exam/DNDAnswerContainer'
 import { ComponentWrapper } from '../utils/ComponentWrapper'
+import parseExam from '../../../src/parser/parseExam'
+import { query } from '../../../src/dom-utils'
 
 interface DNDAnswerContainerStoryProps {
-  content: string
+  masteredExam: MasteringResult[]
   exam: string
 }
 
-export const DNDAnswerContainerStory: React.FC<DNDAnswerContainerStoryProps> = ({ content, exam }) => {
-  const parser = new DOMParser()
-  const element = parser.parseFromString(content, 'application/xml').documentElement
+export const DNDAnswerContainerStory: React.FC<DNDAnswerContainerStoryProps> = ({ masteredExam, exam }) => {
+  const doc = parseExam(masteredExam[0].xml, true)
+  const element = query(doc.documentElement, 'dnd-answer-container')!
   return (
     <ComponentWrapper exam={exam}>
       <DNDAnswerContainer element={element} renderChildNodes={renderChildNodes}></DNDAnswerContainer>
