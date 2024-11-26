362bae624428512c858ab241267df210f0857a34
Heikki Mantere <heikki.mantere@gmail.com>
2024-11-26 13:00:17 +0200

    Both answer title and droppable container are droppable

--- packages/core/src/components/results/DNDAnswer.tsx
+++ packages/core/src/components/results/DNDAnswer.tsx
@@ -38,6 +38,7 @@ export const DNDAnswer = ({
         items={items}
         answerOptionsByQuestionId={answerOptionsByQuestionId}
         correctIds={correctOptionIds}
+        questionId={questionId}
       />
 
       <ResultsExamQuestionAutoScore
