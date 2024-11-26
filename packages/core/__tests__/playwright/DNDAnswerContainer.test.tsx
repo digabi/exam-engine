baf032d5bb86a6978b6e32c323058fa85bf38bdb
Heini <ahokas.heini@gmail.com>
2024-11-26 17:17:59 +0200

    tests(core): expand dnd answer container tests

--- packages/core/__tests__/playwright/DNDAnswerContainer.test.tsx
+++ packages/core/__tests__/playwright/DNDAnswerContainer.test.tsx
@@ -3,20 +3,41 @@ import { readFileSync } from 'fs'
 import path from 'path'
 import { test, expect } from '@playwright/experimental-ct-react'
 import { resolveExam } from '@digabi/exam-engine-exams'
-import { getMediaMetadataFromLocalFile, masterExam } from '@digabi/exam-engine-mastering'
+import { getMediaMetadataFromLocalFile, masterExam, MasteringResult } from '@digabi/exam-engine-mastering'
 import { DNDAnswerContainerStory } from './stories/DNDAnswerContainer.story'
 
 test.describe('DNDAnswerContainer', () => {
-  test('renders DNDAnswerContainer correctly', async ({ mount }) => {
+  let examXml: string
+  let masteredExam: MasteringResult
+
+  test.beforeAll(async () => {
     const examPath = resolveExam('SC/SC.xml')
     const resolveAttachment = (filename: string) => path.resolve(path.dirname(examPath), 'attachments', filename)
-    const source = readFileSync(examPath, 'utf-8')
-    const results = await masterExam(source, () => '', getMediaMetadataFromLocalFile(resolveAttachment), {
+    examXml = readFileSync(examPath, 'utf-8')
+    ;[masteredExam] = await masterExam(examXml, () => '', getMediaMetadataFromLocalFile(resolveAttachment), {
       removeCorrectAnswers: false
     })
-    const component = await mount(<DNDAnswerContainerStory masteredExam={results} exam={source} />)
-
-    // Add your assertions here
-    await expect(component).toBeVisible()
   })
+
+  for (const answerMediaType of ['text', 'image'] as const) {
+    test(`Connect - basic drag and drop functionality for ${answerMediaType} answers`, async ({ mount }) => {
+      const component = await mount(
+        <DNDAnswerContainerStory masteredExam={masteredExam} answerMediaType={answerMediaType} />
+      )
+      const answerOption = component.locator('.e-dnd-answer-option').first()
+      const answerOptionTextContent = await answerOption.textContent()
+      const draggable = answerOption.locator('.drag-handle')
+      const droppable = component.locator('.e-dnd-answer-droppable').first()
+
+      await test.step('Answer option can be dragged from answer options to an answer container ', async () => {
+        await draggable.dragTo(droppable)
+        await expect(droppable).toContainText(answerOptionTextContent as string)
+        if (answerMediaType === 'image') {
+          await expect(droppable.locator('img')).toBeVisible()
+        }
+      })
+      await test.step('Answer option gets replaced when other answer option gets dragged into an answer container ', async () => {})
+      await test.step('Answer option can get dragged back from answer container to options container ', async () => {})
+    })
+  }
 })
