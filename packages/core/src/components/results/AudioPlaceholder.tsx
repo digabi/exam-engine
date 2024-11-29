8d838b754e5e40ab1898bfb7c53d4414ec98deab
Heikki Mantere <heikki.mantere@gmail.com>
2024-11-29 13:07:37 +0200

    Style restricted player in Results page to look like in exam

--- packages/core/src/components/results/AudioPlaceholder.tsx
+++ packages/core/src/components/results/AudioPlaceholder.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { getNumericAttribute } from '../../dom-utils'
-import { formatDuration } from '../shared/internal/RestrictedAudioPlayer'
+import { audioLabelId } from '../../ids'
 import Audio from '../shared/Audio'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { formatDuration } from '../shared/internal/RestrictedAudioPlayer'
 
 export const AudioPlaceholder: React.FC<ExamComponentProps> = ({ element, renderChildNodes }) => {
   const restricted = !!getNumericAttribute(element, 'times')
   const duration = getNumericAttribute(element, 'duration')!
-  const formattedDuration = formatDuration(duration)
-  console.log(restricted)
+  const labelId = audioLabelId(element)
 
   if (restricted) {
-    return <div className="audio-placeholder">Rajoitettu kuuntelu – {formattedDuration}</div>
+    return (
+      <div className="audio e-columns e-columns--center-v e-mrg-b-2">
+        <div className="e-pad-1 restricted-audio-player restricted-audio-player--disabled e-columns e-columns--center-v">
+          <button
+            className="restricted-audio-player__play e-column e-column--narrow"
+            disabled={true}
+            onClick={() => undefined}
+          >
+            <FontAwesomeIcon icon={faPlay} fixedWidth />
+          </button>
+          <span className="e-column e-text-right e-column">{formatDuration(duration)}</span>
+        </div>
+
+        <span className="e-pad-l-2" id={labelId}>
+          <em>Ei kuunneltavissa</em>
+        </span>
+      </div>
+    )
   } else {
     return <Audio element={element} renderChildNodes={renderChildNodes} />
   }
