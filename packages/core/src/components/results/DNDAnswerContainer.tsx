39a6e15b24a9bf6d97a29e0455317ef93a4ef60b
Heikki Mantere <heikki.mantere@gmail.com>
2024-12-04 22:12:31 +0200

    Refactor two DNDAnswerContainers to use common utils

--- packages/core/src/components/results/DNDAnswerContainer.tsx
+++ packages/core/src/components/results/DNDAnswerContainer.tsx
@@ -1,46 +1,25 @@
-import { UniqueIdentifier } from '@dnd-kit/core'
 import React, { useContext } from 'react'
 import { ExamComponentProps } from '../..'
 import { query, queryAll } from '../../dom-utils'
 import { ResultsContext } from '../context/ResultsContext'
+import { getAnswerOptionIdsByQuestionId, getAnswerOptionsByOptionId } from '../exam/DNDAnswerContainer'
 import { DNDTitleAndDroppable } from '../shared/DNDTitleAndDroppable'
 import { CorrectDNDAnswers } from './CorrectDNDAnswers'
-import { ItemsState } from '../exam/DNDAnswerContainer'
 
 export const DNDAnswerContainer = ({ element, renderChildNodes }: ExamComponentProps) => {
   const { answersByQuestionId, gradingStructure } = useContext(ResultsContext)
-  const dndAnswers = queryAll(element, 'dnd-answer').filter(e => !!query(e, 'dnd-answer-title'))
-  const dndAnswerOptions = queryAll(element, 'dnd-answer-option')
-
-  const answerOptionIdsByQuestionId: ItemsState = dndAnswers.reduce(
-    (acc, group) => {
-      const questionId = group.getAttribute('question-id')!
-      const answer = answersByQuestionId?.[Number(questionId)]?.value
-      return {
-        ...acc,
-        [questionId]: answer ? [Number(answer)] : [],
-        root: acc.root.filter(e => e !== Number(answer))
-      }
-    },
-    { root: dndAnswerOptions.map(e => Number(e.getAttribute('option-id')!)) }
-  )
-
-  const answerOptionsById: Record<UniqueIdentifier, Element> = dndAnswerOptions.reduce((acc, el) => {
-    const optionId = el.getAttribute('option-id')!
-    return { ...acc, [optionId]: el }
-  }, {})
-
+  const answerOptionIdsByQuestionId = getAnswerOptionIdsByQuestionId(element, answersByQuestionId)
+  const answerOptionsByOptionId = getAnswerOptionsByOptionId(element)
   const dndAnswersWithQuestion = queryAll(element, 'dnd-answer').filter(e => !!query(e, 'dnd-answer-title'))
 
   return (
     <div className="e-dnd-answer-container">
       {dndAnswersWithQuestion.map(element => {
         const questionId = element.getAttribute('question-id')!
-
-        const answerOptionElements = (answerOptionIdsByQuestionId[questionId] || [])
-          ?.map(id => answerOptionsById?.[id] || null)
-          .filter(Boolean)
         const itemIds = answerOptionIdsByQuestionId[questionId] || []
+        const answerOptionElements = (answerOptionIdsByQuestionId[questionId] || [])
+          ?.map(id => answerOptionsByOptionId?.[id] || null)
+          .filter(Boolean)
 
         return (
           <React.Fragment key={questionId}>
@@ -58,7 +37,7 @@ export const DNDAnswerContainer = ({ element, renderChildNodes }: ExamComponentP
                 key={`${questionId}-correct`}
                 element={element}
                 renderChildNodes={renderChildNodes}
-                answerOptionsById={answerOptionsById}
+                answerOptionsById={answerOptionsByOptionId}
               />
             )}
           </React.Fragment>
