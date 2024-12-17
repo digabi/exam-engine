2f3ad661bff77fb0a31f082859eb95ff7bfa4b6a
Heini <ahokas.heini@gmail.com>
2024-12-17 15:57:58 +0200

    refactor(tests): move helper function to a util

---
+++ packages/core/__tests__/playwright/utils/utils.ts
@@ -0,0 +1,14 @@
+import { resolveExam } from '@digabi/exam-engine-exams'
+import { readFileSync } from 'fs'
+import path from 'path'
+import { getMediaMetadataFromLocalFile, masterExam, MasteringResult } from '@digabi/exam-engine-mastering'
+
+export async function setupMasteredExam(subject: string = 'SC'): Promise<MasteringResult> {
+  const examPath = resolveExam(`${subject}/${subject}.xml`)
+  const resolveAttachment = (filename: string) => path.resolve(path.dirname(examPath), 'attachments', filename)
+  const examXml = readFileSync(examPath, 'utf-8')
+  const [masteredExam] = await masterExam(examXml, () => '', getMediaMetadataFromLocalFile(resolveAttachment), {
+    removeCorrectAnswers: true
+  })
+  return masteredExam
+}
