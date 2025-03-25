d425b1a73230d0a35f356a9c22dd8d1b68ec0831
Aleksi Vuorjoki <aleksi.vuorjoki@reaktor.com>
2025-03-25 17:26:32 +0200

    Add saving audio answers to examServerApi

--- packages/core/src/components/exam/AudioAnswer.tsx
+++ packages/core/src/components/exam/AudioAnswer.tsx
@@ -2,16 +2,20 @@ import React, { useContext, useState } from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { getNumericAttribute } from '../../dom-utils'
 import { saveAnswer } from '../../store/answers/actions'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { ReactMediaRecorder } from 'react-media-recorder'
 import { ExamContext } from '../context/ExamContext'
+import { AnswersState } from '../../store/answers/reducer'
+import type { AudioAnswer } from '../../types/ExamAnswer'
 
 interface AudioAnswerRecorderProps {
+  audioUrl?: string
   onSave: (blob: Blob) => void
+  onDelete: () => void
   bitsPerSecond?: number
 }
 
-function AudioAnswerRecorder({ onSave, bitsPerSecond }: AudioAnswerRecorderProps) {
+function AudioAnswerRecorder({ onSave, onDelete, bitsPerSecond, audioUrl }: AudioAnswerRecorderProps) {
   const [blobSize, setBlobSize] = useState<number>(0)
 
   return (
@@ -19,14 +23,15 @@ function AudioAnswerRecorder({ onSave, bitsPerSecond }: AudioAnswerRecorderProps
       audio={true}
       mediaRecorderOptions={{ audioBitsPerSecond: bitsPerSecond ?? 65536 }}
       onStop={(_blobUrl, blob) => onSave(blob)}
-      render={({ status, error, startRecording, stopRecording, clearBlobUrl, mediaBlobUrl }) => (
+      render={({ status, error, startRecording, stopRecording }) => (
         <>
           <div>
             <p>status: {status}</p>
             {error && <p>error: {error}</p>}
             {blobSize > 0 && <p>size: {blobSize} bytes</p>}
+            {audioUrl && <p>audiourl: {audioUrl}</p>}
             <p>
-              <button onClick={startRecording} disabled={status != 'idle'}>
+              <button onClick={startRecording} disabled={status != 'idle' || !!audioUrl}>
                 Tallenna ääntä
               </button>
               <button onClick={stopRecording} disabled={status != 'recording'}>
@@ -34,18 +39,18 @@ function AudioAnswerRecorder({ onSave, bitsPerSecond }: AudioAnswerRecorderProps
               </button>
               <button
                 onClick={() => {
-                  clearBlobUrl()
                   setBlobSize(0)
+                  onDelete()
                 }}
-                disabled={status != 'stopped' || !mediaBlobUrl}
+                disabled={!audioUrl}
               >
                 Poista äänite
               </button>
             </p>
           </div>
-          {mediaBlobUrl && (
+          {audioUrl && (
             <audio
-              src={mediaBlobUrl}
+              src={audioUrl}
               className="e-column e-column--narrow"
               preload="metadata"
               controls
@@ -60,7 +65,11 @@ function AudioAnswerRecorder({ onSave, bitsPerSecond }: AudioAnswerRecorderProps
 
 function AudioAnswer(audioAnswerProps: ExamComponentProps) {
   const { element } = audioAnswerProps
+
   const questionId = getNumericAttribute(element, 'question-id')!
+  const answer = useSelector(
+    (state: { answers: AnswersState }) => state.answers.answersById[questionId] as AudioAnswer | undefined
+  )
   const displayNumber = element.getAttribute('display-number')!
   const dispatch = useDispatch()
   const { examServerApi } = useContext(ExamContext)
@@ -69,9 +78,15 @@ function AudioAnswer(audioAnswerProps: ExamComponentProps) {
     <>
       <span className="anchor" id={`question-nr-${displayNumber}`} />
       <AudioAnswerRecorder
+        audioUrl={answer?.value === '' ? undefined : answer?.value}
         onSave={async audio => {
           const audioAttachmentUrl = await examServerApi.saveAudio(questionId, audio)
           const answer = { questionId, type: 'audio' as const, value: audioAttachmentUrl }
+          dispatch(saveAnswer(answer))
+        }}
+        onDelete={async () => {
+          await examServerApi.deleteAudio(questionId)
+          const answer = { questionId, type: 'audio' as const, value: '' }
           dispatch(saveAnswer(answer))
         }}
       />
