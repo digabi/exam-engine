58668ac370d92ed348474e17f031af2162d17bd6
Petri Ämmälä <petri.ammala@reaktor.com>
2025-04-16 14:19:51 +0300

    Show scores and comments in results

--- packages/core/src/components/results/AudioAnswer.tsx
+++ packages/core/src/components/results/AudioAnswer.tsx
@@ -1,18 +1,37 @@
 import React, { useContext } from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { getNumericAttribute } from '../../dom-utils'
-import { ResultsContext } from '../context/ResultsContext'
+import { findScore, ResultsContext } from '../context/ResultsContext'
 import { TextAnswer } from '../../types/ExamAnswer'
+import ResultsExamQuestionManualScore from './internal/QuestionManualScore'
+import { shortDisplayNumber } from '../../shortDisplayNumber'
+import { QuestionContext } from '../context/QuestionContext'
+import { useExamTranslation } from '../../i18n'
 
 function AudioAnswer(audioAnswerProps: ExamComponentProps) {
   const { element } = audioAnswerProps
-  const { answersByQuestionId } = useContext(ResultsContext)
+  const { answers } = useContext(QuestionContext)
+  const { t } = useExamTranslation()
+  const { answersByQuestionId, scores } = useContext(ResultsContext)
   const questionId = getNumericAttribute(element, 'question-id')!
   const answer = answersByQuestionId[questionId] as TextAnswer | undefined
   const value = answer && answer.value
+  const score = findScore(scores, questionId)
+  const maxScore = getNumericAttribute(element, 'max-score')!
+  const displayNumber = answers.length > 1 ? shortDisplayNumber(element.getAttribute('display-number')!) : undefined
+  const comment = score?.pregrading?.comment
 
   return value ? (
-    <audio src={value} className="e-column e-column--narrow" preload="metadata" controls controlsList="nodownload" />
+    <>
+      <audio src={value} className="e-column e-column--narrow" preload="metadata" controls controlsList="nodownload" />
+      <ResultsExamQuestionManualScore {...{ scores: score, maxScore, displayNumber, questionId }} />
+      {comment && (
+        <>
+          <h5>{t('comment')}</h5>
+          <p className="e-italic">{comment}</p>
+        </>
+      )}
+    </>
   ) : null
 }
 
