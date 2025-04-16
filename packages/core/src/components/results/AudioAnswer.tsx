092f51d34c901bc4658ceb3e0c48db864129a8a7
Petri Ämmälä <petri.ammala@reaktor.com>
2025-04-16 14:19:51 +0300

    Show scores and comments in results

--- packages/core/src/components/results/AudioAnswer.tsx
+++ packages/core/src/components/results/AudioAnswer.tsx
@@ -1,19 +1,52 @@
 import React, { useContext } from 'react'
 import { ExamComponentProps } from '../../createRenderChildNodes'
 import { getNumericAttribute } from '../../dom-utils'
-import { ResultsContext } from '../context/ResultsContext'
+import { findScore, ResultsContext } from '../context/ResultsContext'
 import { TextAnswer } from '../../types/ExamAnswer'
+import ResultsExamQuestionManualScore from './internal/QuestionManualScore'
+import { shortDisplayNumber } from '../../shortDisplayNumber'
+import { QuestionContext } from '../context/QuestionContext'
+import { useExamTranslation } from '../../i18n'
+import classNames from 'classnames'
 
 function AudioAnswer(audioAnswerProps: ExamComponentProps) {
   const { element } = audioAnswerProps
-  const { answersByQuestionId } = useContext(ResultsContext)
+  const { answers } = useContext(QuestionContext)
+  const { t, i18n } = useExamTranslation()
+  const { answersByQuestionId, scores } = useContext(ResultsContext)
   const questionId = getNumericAttribute(element, 'question-id')!
   const answer = answersByQuestionId[questionId] as TextAnswer | undefined
   const value = answer && answer.value
+  const score = findScore(scores, questionId)
+  const maxScore = getNumericAttribute(element, 'max-score')!
+  const displayNumber = answers.length > 1 ? shortDisplayNumber(element.getAttribute('display-number')!) : undefined
+  const comment = score?.pregrading?.comment
 
-  return value ? (
-    <audio src={value} className="e-column e-column--narrow" preload="metadata" controls controlsList="nodownload" />
-  ) : null
+  return (
+    <>
+      <ResultsExamQuestionManualScore {...{ scores: score, maxScore, displayNumber, questionId }} />
+      <div
+        className={classNames('audio-answer', {
+          'no-answer': !value
+        })}
+        aria-description={!value ? i18n.t('examineExam.questionHasNoAnswer') : undefined}
+      >
+        <audio
+          src={value}
+          className="e-column e-column--narrow"
+          preload="metadata"
+          controls
+          controlsList="nodownload"
+        />
+      </div>
+      {comment && (
+        <>
+          <h5>{t('comment')}</h5>
+          <p className="e-italic">{comment}</p>
+        </>
+      )}
+    </>
+  )
 }
 
 export default React.memo(AudioAnswer)
