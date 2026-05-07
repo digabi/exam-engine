298afdff17ff96e187f76ebe32e63203f5f64fc0
Aleksi Ahonen <aleksi.ahonen@reaktor.com>
2026-05-07 09:13:51 +0300

    Do not spam console.error if fonts fail to load

--- packages/core/src/components/exam/DropdownAnswer.tsx
+++ packages/core/src/components/exam/DropdownAnswer.tsx
@@ -47,9 +47,7 @@ const DropdownAnswer: React.FunctionComponent<ExamComponentProps> = ({ element, 
   if (runningInBrowser) {
     // Force a re-measure if element changes or fonts are loaded after this
     // component has been rendered.
-    fonts.ready
-      .then(() => !fontsLoaded && setFontsLoaded(true))
-      .catch(err => console.error('Loading fonts failed', err))
+    fonts.ready.then(() => !fontsLoaded && setFontsLoaded(true)).catch(() => {})
 
     useEffect(() => setMeasuring(true), [element])
 
