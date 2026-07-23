ea2654622a8e3cc321caf0970f3c3e149caa152e
Ismo Muusavi <ismo.muusavi@reaktor.com>
2026-07-23 10:13:22 +0300

    Show "save failed, too long answer" error in ErrorIndicator

--- packages/core/src/components/exam/internal/SaveIndicator.tsx
+++ packages/core/src/components/exam/internal/SaveIndicator.tsx
@@ -10,19 +10,13 @@ function SaveIndicator() {
   const tooLongFailureDisplayNumber = useSelector(getAnyAnswerTooLongFailure)
   const { t } = useExamTranslation()
 
-  if (state === 'initial' && !tooLongFailureDisplayNumber) {
+  if (state === 'initial' || tooLongFailureDisplayNumber) {
     return null
   }
 
   return (
     <dialog open className="save-indicator e-pad-1 e-font-size-xs">
-      {tooLongFailureDisplayNumber ? (
-        <span className="save-indicator-text" role="alert">
-          {t('answer-errors.save-failed-too-long', { displayNumber: tooLongFailureDisplayNumber })}
-        </span>
-      ) : (
-        <span className={classNames('save-indicator-text', `save-indicator-text--${state}`)}>{t('answer-saved')}</span>
-      )}
+      <span className={classNames('save-indicator-text', `save-indicator-text--${state}`)}>{t('answer-saved')}</span>
     </dialog>
   )
 }
